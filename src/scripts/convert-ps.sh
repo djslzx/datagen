12f44c6c84580f14bb6b1d7fa49eeaa9ffceb956
David J. Lee <djslzx@gmail.com>
2022-08-22 03:35:10 -0400

    fix convert wildcard

--- src/scripts/convert-ps.sh
+++ src/scripts/convert-ps.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 RENDER_DIR="$1"
-for file in "${RENDER_DIR}/*/*.ps"
-do 
-  echo "Processing file ${file}..."
-  ./scripts/ps2png.sh $file ${file}.png 2> /dev/null
-done
+if [[ -z "$RENDER_DIR" ]]; then
+   echo "Usage: convert-ps.sh RENDER_DIR"
+   exit 1
+fi
 
-for file in "${RENDER_DIR}/*.ps"
+# for file in "${RENDER_DIR}/*/*.ps"
+# do 
+#   echo "Processing file ${file}..."
+#   ./scripts/ps2png.sh $file ${file}.png 2> /dev/null
+# done
+for file in "${RENDER_DIR}"/*.ps
 do 
   echo "Processing file ${file}..."
   ./scripts/ps2png.sh $file ${file}.png 2> /dev/null
