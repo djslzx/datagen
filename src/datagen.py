a81dea6b68eede70b58acca5069044bdb313fb28
David J. Lee <djslzx@gmail.com>
2022-08-25 15:30:57 -0400

    refactor datagen main()

--- src/datagen.py
+++ src/datagen.py
@@ -2,7 +2,7 @@ import random
 import pickle
 import argparse
 import multiprocess as mp
-from lindenmayer import S0LSystem, CFG
+from lindenmayer import S0LSystem, CFG, LSystem
 import util
 
 
@@ -122,6 +122,87 @@ def general_random_S0LSystem(n_rules: int,
                               max_rule_length)
 
 
+def make_grammar(args, index: int, log=True) -> S0LSystem:
+    g = constrained_random_S0LSystem(
+        n_rules=random.randint(*args.n_rules),
+        max_axiom_length=random.randint(*args.axiom_length),
+        max_rule_length=random.randint(*args.rule_length),
+    )
+    print(f"[{index}] {g}")
+
+    if log:
+        # save grammar text representation to text file for debug
+        with open(f'{args.out_dir}/grammars.log', 'a') as f:
+            f.write(f'Grammar {index}: {g}\n')
+
+    # save grammar object for model training
+    with open(f'{args.out_dir}/grammars.dat', 'ab') as f:
+        pickle.dump((index, g), f)
+
+    return g
+
+
+def make_word(args, grammar: S0LSystem, grammar_i: int, specimen_i: int,
+              angle: float, verbose=False, log=True) -> str:
+    word = grammar.nth_expansion(args.devel_depth)
+
+    if verbose:
+        word_preview = word[:20] + ("..." if len(word) > 20 else "")
+        print(f"[{grammar_i}, {specimen_i}] "
+              f"Generated word {word_preview} of length {len(word)}")
+
+    if log:
+        # write debug text
+        with open(f'{args.out_dir}/words.log', 'a') as f:
+            f.write(f'[{grammar_i}, {specimen_i}, {angle}]: {word}\n')
+
+    # write object
+    with open(f'{args.out_dir}/words.dat', 'ab') as f:
+        pickle.dump((grammar_i, specimen_i, angle, word), f)
+
+    return grammar_i, specimen_i, word, angle
+
+
+def make_sticks(args, grammar: int, specimen: int, word: str, angle: float,
+                verbose=False, log=True):
+    if verbose:
+        word_preview = word[:20] + ("..." if len(word) > 20 else "")
+        print(f"[{grammar}, {specimen}] "
+              f"Rendering {word_preview} of length {len(word)}")
+
+    sticks = S0LSystem.to_sticks(
+        word,
+        d=10,
+        theta=angle,
+    )
+
+    if log:
+        with open(f'{args.out_dir}/sticks.log', 'a') as f:
+            s = "\n  ".join(str(stick) for stick in sticks)
+            f.write(f'{grammar, specimen, angle}: [\n  {s}\n]\n')
+
+    with open(f'{args.out_dir}/sticks.dat', 'ab') as f:
+        pickle.dump((grammar, specimen, angle, sticks), f)
+
+    LSystem.to_svg(
+        sticks,
+        f'{args.out_dir}/render[{grammar},{specimen}]@{angle}deg.svg'
+    )
+
+
+def make_render(args, id: str, word: str, angle: float, verbose=False):
+    if verbose:
+        word_preview = word[:20] + ("..." if len(word) > 20 else "")
+        print(f"[{id}] Rendering {word_preview} of length {len(word)}")
+
+    S0LSystem.render_with_turtle(
+        word,
+        d=10,
+        theta=angle,
+        filename=f'{args.out_dir}/render[{id}]{angle}deg.dat'
+    )
+
+
 if __name__ == '__main__':
     p = argparse.ArgumentParser(description="Generate L-systems.")
     p.add_argument('n_grammars', type=int,
@@ -141,77 +222,32 @@ if __name__ == '__main__':
     args = p.parse_args()
     angles = [random.randint(10, 90) for _ in range(5)]
 
-    def make_grammar(index: int, log=True) -> S0LSystem:
-        g = constrained_random_S0LSystem(
-            n_rules=random.randint(*args.n_rules),
-            max_axiom_length=random.randint(*args.axiom_length),
-            max_rule_length=random.randint(*args.rule_length),
-        )
-        print(f"[{index}] {g}")
-
-        if log:
-            # save grammar text representation to text file for debug
-            with open(f'{args.out_dir}/grammars.log', 'a') as f:
-                f.write(f'Grammar {index}: {g}')
-
-        # save grammar object for model training
-        with open(f'{args.out_dir}/grammars.dat', 'ab') as f:
-            pickle.dump((index, g), f)
-
-        return g
-
-    def make_word(grammar: S0LSystem, grammar_i: int, specimen_i: int,
-                  angle: float, verbose=False, log=True) -> str:
-        word = grammar.nth_expansion(args.devel_depth)
-
-        if verbose:
-            word_preview = word[:20] + ("..." if len(word) > 20 else "")
-            print(f"[{grammar_i}, {specimen_i}] "
-                  f"Generated word {word_preview} of length {len(word)}")
-
-        if log:
-            # write debug text
-            with open(f'{args.out_dir}/words.log', 'a') as f:
-                f.write(f'[{grammar_i}, {specimen_i}, {angle}]: {word}\n')
-
-        # write object
-        with open(f'{args.out_dir}/words.dat', 'ab') as f:
-            pickle.dump((grammar_i, specimen_i, angle, word), f)
-
-        return grammar_i, specimen_i, word, angle
-
-    def make_render(grammar: int, specimen: int, word: str, angle: float,
-                    verbose=False):
-        if verbose:
-            word_preview = word[:20] + ("..." if len(word) > 20 else "")
-            print(f"[{grammar}, {specimen}] "
-                  f"Rendering {word_preview} of length {len(word)}")
-
-        S0LSystem.render(
-            word,
-            length=10,
-            angle=angle,
-            filename=(f'{args.out_dir}/'
-                      f'endpoint[{grammar},{specimen}]'
-                      f'{angle}deg'),
-        )
-
     with mp.Pool() as pool:
         print("Making grammars...")
-        grammars = pool.imap(make_grammar, range(args.n_grammars))
+        grammars = pool.imap(lambda n: make_grammar(args, n),
+                             range(args.n_grammars))
 
         # render endpoint
         print("Making endpoint words...")
-        words = pool.starmap(lambda grammar, i, j, angle:
-                             make_word(grammar, i, j, angle, verbose=True),
-                             ((grammar, i, j, a)
-                              for i, grammar in enumerate(grammars)
-                              for j in range(args.n_specimens)
-                              for a in angles))
+        words = pool.starmap(
+            lambda grammar, i, j, theta:
+            make_word(args, grammar, i, j, theta, verbose=True),
+            ((grammar, i, j, theta)
+             for i, grammar in enumerate(grammars)
+             for j in range(args.n_specimens)
+             for theta in angles)
+        )
 
-        print("Rendering endpoint words...")
+        print("Rendering endpoint words to sticks...")
         pool.starmap(
             lambda grammar, specimen, word, angle:
-            make_render(grammar, specimen, word, angle, verbose=True),
+            make_sticks(args, grammar, specimen, word, angle, verbose=True),
             words
         )
+
+        # print("Rendering endpoint words to images...")
+        # pool.starmap(
+        #     lambda grammar, specimen, word, angle:
+        #     make_render(args, (grammar, specimen), word, angle, verbose=True),
+        #     words
+        # )
