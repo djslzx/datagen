ea2ab50f412054dd50e81091e763ccfd7c0a0c71
David J. Lee <djslzx@gmail.com>
2022-09-01 13:37:33 -0400

    ignore missing postscript; don't add *.ps

--- src/scripts/convert-ps.sh
+++ src/scripts/convert-ps.sh
@@ -10,9 +10,12 @@ fi
 #   echo "Processing file ${file}..."
 #   ./scripts/ps2png.sh $file ${file}.png 2> /dev/null
 # done
-for file in "${DIR}"/*.ps
-do 
-  echo "Processing file ${file}..."
-  ./scripts/ps2png.sh "$file" "${file}.png" 2> /dev/null
-done
-
+if ls "${DIR}/*.ps" > /dev/null 2>&1; then
+  for file in "${DIR}"/*.ps
+   do 
+     echo "Processing file ${file}..."
+     ./scripts/ps2png.sh "$file" "${file}.png" 2> /dev/null
+   done
+else
+  echo "No postscript files found"
+fi
