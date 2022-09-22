391b365dc8e5a0bd384e995a4ccb40a4200e7e02
David J. Lee <djslzx@gmail.com>
2022-09-22 13:48:47 -0400

    split zoo

--- src/zoo.py
+++ src/zoo.py
@@ -5,239 +5,8 @@ import sys
 import random
 from typing import List, Tuple
 from lindenmayer import S0LSystem
-
-# List of specimens and their angles
-ZOO: List[Tuple[S0LSystem, float]] = [
-    # Deterministic L-systems
-    (S0LSystem(
-        axiom="F-F-F-F",
-        productions={
-            "F": ["F+FF-FF-F-F+F+F"
-                  "F-F-F+F+FF+FF-F"]
-        },
-        distribution="uniform",
-    ), 90),
-    (S0LSystem(
-        axiom="-F",
-        productions={
-            "F": ["F+F-F-F+F"]
-        },
-        distribution="uniform",
-    ), 90),
-    (S0LSystem(
-        axiom="F+F+F+F",
-        productions={
-            "F": ["F+f-FF+F+FF+Ff+FF-f+FF-F-FF-Ff-FFF"],
-            "f": ["fffff"]
-        },
-        distribution="uniform",
-    ), 90),
-    (S0LSystem(
-        axiom="F-F-F-F",
-        productions={
-            "F": ["FF-F-F-F-F-F+F"]
-        },
-        distribution="uniform",
-    ), 90),
-    (S0LSystem(
-        axiom="F-F-F-F",
-        productions={
-            "F": ["FF-F-F-F-FF"]
-        },
-        distribution="uniform",
-    ), 90),
-    (S0LSystem(
-        axiom="F-F-F-F",
-        productions={
-            "F": ["FF-F+F-F-FF"]
-        },
-        distribution="uniform",
-    ), 90),
-    (S0LSystem(
-        axiom="F-F-F-F",
-        productions={
-            "F": ["FF-F--F-F"]
-        },
-        distribution="uniform",
-    ), 90),
-    (S0LSystem(
-        axiom="F-F-F-F",
-        productions={
-            "F": ["F-FF--F-F"]
-        },
-        distribution="uniform",
-    ), 90),
-    (S0LSystem(
-        axiom="F-F-F-F",
-        productions={
-            "F": ["F-F+F-F-F"]
-        },
-        distribution="uniform",
-    ), 90),
-    # Left- right- rules
-    (S0LSystem(
-        axiom="F",
-        productions={
-            "F": ["F+F+",
-                  "-F-F"]
-        },
-        distribution="uniform",
-    ), 90),
-    (S0LSystem(
-        axiom="F",
-        productions={
-            "F": ["F+F+F",
-                  "F-F-F"]
-        },
-        distribution="uniform",
-    ), 60),
-    # Rewriting techniques
-    (S0LSystem(
-        axiom="F",
-        productions={
-            "F": ["F+F++F-F--FF-F+",
-                  "-F+FF++F+F--F-F"]
-        },
-        distribution="uniform",
-    ), 90),
-    (S0LSystem(
-        axiom="-F",
-        productions={
-            "F": ["FF-F-F+F+F-F-FF""+F+FFF-F+F+FF+F-FF-F-F+F+FF",
-                  "+FF-F-F+F+FF+FFF-F-F+FFF-F-FF+F+F-F-F+F+FF"]
-        },
-        distribution="uniform",
-    ), 90),
-    (S0LSystem(
-        axiom="-L",
-        productions={
-            "L": ["LF+RFR+FL-F-LFLFL-FRFR+"],
-            "R": ["-LFLF+RFRFR+F+RF-LFL-FR"],
-        },
-        distribution="uniform",
-    ), 90),
-    (S0LSystem(
-        axiom="-L",
-        productions={
-            "L": ["LFLF+RFR+FLFL-FRF-LFL-FR+F+RF-LFL-FRFRFR+"],
-            "R": ["-LFLFLF+RFR+FL-F-LF+RFR+FLF+RFRF-LFL-FRFR"],
-        },
-        distribution="uniform",
-    ), 90),
-    (S0LSystem(
-        axiom="L",
-        productions={
-            "L": ["LFRFL-F-RFLFR+F+LFRFL"],
-            "R": ["RFLFR+F+LFRFL-F-RFLFR"]
-        },
-        distribution="uniform",
-    ), 90),
-    # Branching w/ edge rewriting
-    (S0LSystem(
-        axiom="F",
-        productions={
-            "F": ["F[+F]F[-F]F"]
-        },
-        distribution="uniform",
-    ), 25.7),
-    (S0LSystem(
-        axiom="F",
-        productions={
-            "F": ["F[+F]F[-F][F]"]
-        },
-        distribution="uniform",
-    ), 20),
-    (S0LSystem(
-        axiom="F",
-        productions={
-            "F": ["FF-[-F+F+F]+[+F-F-F]"]
-        },
-        distribution="uniform",
-    ), 22.5),
-    # Branching with node rewriting
-    (S0LSystem(
-        axiom="X",
-        productions={
-            "X": ["F[+X]F[-X]+X"],
-            "F": ["FF"],
-        },
-        distribution="uniform",
-    ), 20),
-    (S0LSystem(
-        axiom="X",
-        productions={
-            "X": ["F[+X][-X]FX"],
-            "F": ["FF"],
-        },
-        distribution="uniform",
-    ), 25.7),
-    (S0LSystem(
-        axiom="X",
-        productions={
-            "X": ["F-[[X]+X]+F[+FX]-X"],
-            "F": ["FF"],
-        },
-        distribution="uniform",
-    ), 22.5),
-    # Stochastic branching
-    (S0LSystem(
-        axiom="X",
-        productions={
-            "X": ["F[+X]FX",
-                  "F[-X]FX",
-                  "F[+X][-X]FX",
-                  "FFX",
-                  ],
-            "F": ["FF"],
-        },
-        distribution="uniform",
-    ), 20),
-    (S0LSystem(
-        axiom="F",
-        productions={
-            "F": ["F[+F]",
-                  "F[-F]",
-                  "FF"],
-        },
-        distribution="uniform",
-    ), 20),
-    (S0LSystem(
-        axiom="X",
-        productions={
-            "X": ["[+FX][-X]FX",
-                  "[+X][-FX]FX",
-                  "[+FX][-FX]FX"],
-            "F": ["FF"],
-        },
-        distribution="uniform",
-    ), 20),
-    # moss
-    (S0LSystem(
-        axiom="F",
-        productions={
-            "F": ["F[+F]F[-F]F",
-                  "F[+F]F",
-                  "F[-F]F"],
-        },
-        distribution="uniform",
-    ), 20),
-    (S0LSystem(
-        axiom="F",
-        productions={
-            "F": ["F[+F]F[-F]F",
-                  "F[+F]F",
-                  "F[-F]F"],
-        },
-        distribution="uniform",
-    ), 70),
-    # (S0LSystem(
-    #     axiom="",
-    #     productions={
-    #         "F": [""]
-    #     },
-    #     distribution="uniform",
-    # ), 90),
-]
+import book_zoo
+import codex_zoo
 
 
 def mutate_succ(succ: str, rate=0.2) -> str:
@@ -293,7 +62,7 @@ def mate(a: S0LSystem, b: S0LSystem) -> S0LSystem:
 def view_zoo(zoo: List[Tuple[S0LSystem, float]],
              n_specimens: int, expand_length: int, outdir: str):
     for i, (system, theta) in enumerate(zoo):
-        print(f"Handling system {system}...")
+        # print(f"Handling system {system}...")
         words = [system.expand_until(expand_length)
                  for _ in range(n_specimens)]
         for j, (depth, word) in enumerate(words):
@@ -309,5 +78,31 @@ if __name__ == '__main__':
         print("Usage: zoo.py DIR")
         sys.exit(1)
     OUTDIR = sys.argv[1]
-    view_zoo(zoo=ZOO, n_specimens=1, expand_length=1000, outdir=OUTDIR)
+    # view_zoo(zoo=ZOO, n_specimens=1, expand_length=1000, outdir=OUTDIR)
 
+    # check if codex is copying the dataset
+    # for i, (g, _) in enumerate(codex_zoo.zoo):
+    #     for j, (h, _) in enumerate(book_zoo.zoo):
+    #         if g.productions == h.productions:
+    #             print(f"Found a copy at position {i}, {j}: {g}")
+
+    view_zoo(
+        zoo=book_zoo.zoo,
+        n_specimens=1,
+        expand_length=1000,
+        outdir=f"{OUTDIR}/book/"
+    )
+
+    view_zoo(
+        zoo=codex_zoo.zoo,
+        n_specimens=1,
+        expand_length=1000,
+        outdir=f"{OUTDIR}/codex/"
+    )
+
+    view_zoo(
+        zoo=codex_zoo._prompt_zoo,
+        n_specimens=1,
+        expand_length=1000,
+        outdir=f"{OUTDIR}/prompt/"
+    )
