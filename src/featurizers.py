fix resnet input to uint8; lindenmayer.draw outputs uint8; remove scaling factor

src/featurizers.py
@@ -1,10 +1,9 @@
-import pdb
-
 import torch as T
 import numpy as np
 from torchvision.models import resnet50, ResNet50_Weights
 from torchvision.transforms import GaussianBlur
 from typing import *
+from sys import stderr
 import util
 
 
@@ -19,12 +18,11 @@ class Featurizer:
 
 class ResnetFeaturizer(Featurizer):
 
-    def __init__(self, disable_last_layer=False, softmax=True, scaling_factor=1):
-        self.disable_last_layer = disable_last_layer
-        self.scaling_factor = scaling_factor
+    def __init__(self, disable_last_layer=False, softmax=True):
         weights = ResNet50_Weights.DEFAULT
-        self.preprocess = weights.transforms()
         resnet = resnet50(weights=weights)
+        self.preprocess = weights.transforms()
+        self.disable_last_layer = disable_last_layer
         if disable_last_layer:
             self.model = T.nn.Sequential(*list(resnet.children())[:-1])
         else:
@@ -39,15 +37,20 @@ class ResnetFeaturizer(Featurizer):
 
     def apply(self, img: np.ndarray) -> np.ndarray:
         assert len(img.shape) <= 3
-        if self.scaling_factor != 1:
-            img = util.scale_image(img, self.scaling_factor)
+        assert isinstance(img, np.ndarray), f"Expected ndarray, but got {type(img)}"
 
-        # handle alpha channels, grayscale images
+        # resnet only plays nice with uint8 matrices
+        if img.dtype != np.uint8:
+            print(f"WARNING: casting image of type {img.dtype} to uint8", file=stderr)
+            img = img.astype(np.uint8)
+
+        # handle alpha channels, grayscale images -> rgb
         if len(img.shape) == 2:
             img = util.stack_repeat(img, 3)
         elif img.shape[0] != 3:
             img = util.stack_repeat(img[0], 3)
 
+        # run resnet
         batch = self.preprocess(T.from_numpy(img)).unsqueeze(0)
         features = self.model(batch).squeeze()
         if self.softmax:
@@ -56,7 +59,7 @@ class ResnetFeaturizer(Featurizer):
 
     def top_k_classes(self, features: np.ndarray, k: int) -> List[str]:
         top_class_ids = [int(x) for x in (-features).argsort()[:k]]
-        labels = [f"{self.classify(class_id)}: {features[class_id].item(): .4e}"
+        labels = [f"{self.classify(class_id)}: {features[class_id].item(): .4f}"
                   for class_id in top_class_ids]
         return labels
 

src/lindenmayer.py
@@ -107,7 +107,7 @@ class LSystem:
         r, c = n_rows//2, n_cols//2  # start at center of canvas
         heading = 90  # start facing up (logo)
         stack = []
-        canvas = np.zeros((n_rows, n_cols))
+        canvas = np.zeros((n_rows, n_cols), dtype=np.uint8)
         for char in s:
             if char == 'F':
                 r1 = r + int(d * sin(radians(heading)))
@@ -230,12 +230,12 @@ class S0LSystem(LSystem):
         return "".join(self.to_sentence())
 
     @staticmethod
-    def from_sentence(s: List[str]) -> 'S0LSystem':
+    def from_sentence(s: List[str] | Tuple[str]) -> 'S0LSystem':
         """
         Accepts a single string with spaces between distinct tokens, and outputs an L-system.
         The list should have the form 'AXIOM; RULE, RULE, ...', where RULE has the form 'LHS ~ RHS'.
         """
-        assert isinstance(s, list)
+        assert isinstance(s, List) or isinstance(s, Tuple), f"Expected list/tuple of strings but found {type(s)}"
         s = " ".join(s)
         s_axiom, s_rules = s.strip().split(';')
         axiom = s_axiom.replace(' ', '')

src/test_featurizers.py
@@ -1,6 +1,8 @@
+import pdb
+
 from featurizers import *
 from book_zoo import zoo
-from torchvision.io import read_image
+from torchvision.io import read_image, ImageReadMode
 from sklearn.neighbors import NearestNeighbors
 import matplotlib.pyplot as plt
 from typing import *
@@ -12,7 +14,7 @@ def check_resnet_classifier(popn: List[Tuple[S0LSystem, float]], n_samples: int)
     """
     Make sure that resnet classes for each L-system render look reasonable
     """
-    classifier = ResnetFeaturizer(disable_last_layer=False, softmax=True, scaling_factor=10)
+    classifier = ResnetFeaturizer(disable_last_layer=False, softmax=True)
     n = len(popn)
     images = np.empty((n, n_samples, 128, 128))
     labels = np.empty((n, n_samples), dtype=object)
@@ -33,10 +35,11 @@ def check_resnet_with_images(dirpath: str):  # pragma: no cover
     """
     Make sure that resnet gives reasonable answers to stock images
     """
-    classifier = ResnetFeaturizer(disable_last_layer=False, softmax=True)
+    classifier = ResnetFeaturizer()
     for img_file in listdir(dirpath):
         print(f"Testing image {img_file}")
-        img = read_image(f"{dirpath}/{img_file}").numpy()
+        img = read_image(f"{dirpath}/{img_file}", mode=ImageReadMode.GRAY).numpy()
+        print(f"  shape: {img.shape}")
         features = classifier.apply(img)
         labels = classifier.top_k_classes(features, 3)
         plot([img[0]], shape=(1, 1), labels=["\n".join(labels)])
@@ -46,7 +49,7 @@ def check_resnet_featurizer(popn: List[Tuple[S0LSystem, float]], n_samples: int)
     """
     Make sure that the k nearest neighbors of each L-system look reasonable
     """
-    featurizer = ResnetFeaturizer(disable_last_layer=True, softmax=False, scaling_factor=2)  # softmax or not?
+    featurizer = ResnetFeaturizer(disable_last_layer=True, softmax=False)  # softmax or not?
     n = len(popn)
     images = np.empty((n, n_samples, 128, 128))
     features = np.empty((n, n_samples, featurizer.n_features))
@@ -229,7 +232,7 @@ if __name__ == '__main__':  # pragma: no cover
         ][:5]
     ]
 
-    check_resnet_with_images("../resnet-test/")
-    check_resnet_classifier(popn=popn, n_samples=1)
     # check_resnet_preprocess(popn=popn, n_samples=1)
+    # check_resnet_with_images("../resnet-test/screenshots")
+    check_resnet_classifier(popn=popn, n_samples=1)
     # check_resnet_featurizer(popn=popn, n_samples=1)