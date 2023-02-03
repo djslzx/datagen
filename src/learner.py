a1d3ae9e1d7a9cff0ee7b42cc424156fedf910a0
David J. Lee <djslzx@gmail.com>
2023-02-02 22:07:00 -0500

    move types/semantics to parse.py; hook up lsys->str->eggy->str->lsys

--- src/learner.py
+++ src/learner.py
@@ -2,106 +2,52 @@ from __future__ import annotations
 
 import pdb
 
+import lark
 import lightning as L
-import torch.utils.data as data
+import torch.utils.data as Tdata
 from typing import *
 import sys
 from glob import glob
 
 from grammar import Grammar, LearnedGrammar, ConvFeatureExtractor
 from lindenmayer import S0LSystem
+from evo import DRAW_ARGS
 from zoo import zoo
 import parse
 
-Tree: TypeAlias = Tuple
+Tree: TypeAlias = lark.Tree
 
 
-# types of components
-components = {
-    "LSystem_0": ["Axiom", "Rules", "LSystem"],
-    "Nonterminal_0": ["Nonterminal"],  # F
-    "Terminal_0": ["Terminal"],  # +
-    "Terminal_1": ["Terminal"],  # -
-    "Axiom_0": ["Nonterminal", "Axiom", "Axiom"],
-    "Axiom_1": ["Terminal", "Axiom", "Axiom"],
-    "Axiom_2": ["Axiom"],  # empty
-    "Rules_0": ["Rule", "Rules", "Rules"],
-    "Rules_1": ["Rule", "Rules"],
-    "Rule_0": ["Nonterminal", "Rhs", "Rule"],
-    "Rhs_0": ["Rhs", "Rhs", "Rhs"],
-    "Rhs_1": ["Nonterminal", "Rhs", "Rhs"],
-    "Rhs_2": ["Terminal", "Rhs", "Rhs"],
-    "Rhs_3": ["Rhs"],  # empty
-}
-
-# semantics of components
-str_semantics = {
-    "LSystem_0": lambda axiom, rules: f"{axiom};{rules}",
-    "Nonterminal_0": lambda: "F",
-    "Terminal_0": lambda: "+",
-    "Terminal_1": lambda: "-",
-    "Axiom_0": lambda nt, axiom: f"{nt}{axiom}",
-    "Axiom_1": lambda t, axiom: f"{t}{axiom}",
-    "Axiom_2": lambda: "",
-    "Rules_0": lambda r, rs: f"{r},{rs}",
-    "Rules_1": lambda r: r,
-    "Rule_0": lambda nt, rhs: f"{nt}~{rhs}",
-    "Rhs_0": lambda rhs1, rhs2: f"[{rhs1}]{rhs2}",
-    "Rhs_1": lambda nt, rhs: f"{nt}{rhs}",
-    "Rhs_2": lambda t, rhs: f"{t}{rhs}",
-    "Rhs_3": lambda: "",
-}
-
-
-def to_learner_ast(tree: Tree) -> Tree:
-    """
-    Convert tuple node ASTs, where node values are tuples of the form (NT, i),
-    to ASTs with string values.
-    """
-    def transform(*node):
-        symbol, i, *args = node
-        if not args:
-            return f"{symbol}_{i}"
-        else:
-            return f"{symbol}_{i}", *args
-
-    return apply_to_tree(tree, transform)
-
-
-def to_flat_string(ltree: Tree) -> str:
-    if isinstance(ltree, tuple):
-        symbol, *args = ltree
-    else:
-        symbol = ltree
-        args = []
-    str_args = [to_flat_string(arg) for arg in args]
-    return str_semantics[symbol](*str_args)
-
-
-def evaluate(p: Tree):
-    sys_str = to_flat_string(p)
+def eval_ltree_as_lsys(p: Tuple, level=3):
+    sys_str = parse.eval_ttree_as_str(p)
     sys = S0LSystem.from_sentence(list(sys_str))
-    render_str = sys.nth_expansion(3)
-    return S0LSystem.draw(render_str, d=3, theta=45, n_rows=128, n_cols=128)
+    render_str = sys.nth_expansion(level)
+    return S0LSystem.draw(render_str, **DRAW_ARGS)
 
 
-class LSystemDataset(data.Dataset):
+class LSystemDataset(Tdata.Dataset):
     """
     Reads in L-system strings and yields ASTs.
     """
 
     @staticmethod
     def from_files(filenames: List[str]) -> "LSystemDataset":
-        strs = []
+        lines = []
         for filename in filenames:
             with open(filename, "r") as f:
                 for line in f.readlines():
                     if line.startswith("#"):  # skip comments
                         continue
-                    if ":" in line:
+                    if ":" in line:  # split out scores
                         line = line.split(" : ")[0]
-                    strs.append(line.strip())
-        return LSystemDataset(data=strs)
+                    # confirm that line is parseable
+                    try:
+                        parse_str_to_tuple(line)
+                        lines.append(line.strip())
+                    except (lark.UnexpectedCharacters,
+                            lark.UnexpectedToken):
+                        pass
+        return LSystemDataset(data=lines)
 
     def __init__(self, data: List[str]):
         # TODO: allow a program to generate multiple outputs (probabilistic programs)
@@ -110,12 +56,17 @@ class LSystemDataset(data.Dataset):
 
     def __getitem__(self, item):
         s = self.data[item]
-        ast = parse_lsystem_to_ast(s)
-        ast = to_learner_ast(ast)
-        return s, evaluate(ast)
+        ast = parse_str_to_tuple(s)
+        return s, eval_lsys(ast)
 
     def __len__(self):
         return len(self.data)
+
+
+def parse_str_to_tuple(s: str) -> Tuple:
+    """Parses an l-system from an l-system string s"""
+    ltree = parse.parse_lsys_as_ltree(s)
+    return parse.ltree_to_ttree(ltree)
 
 
 if __name__ == "__main__":
@@ -127,19 +78,19 @@ if __name__ == "__main__":
     train_filenames = sorted(glob(train_glob))
     test_filenames = sorted(glob(test_glob))
 
-    # book_examples = [to_learner_ast(parse_lsystem_ast(s.to_code())) for s in zoo]
-    g = Grammar.from_components(components, gram=2)
+    # book_examples = [parse_str_to_tuple(s.to_code()) for s in zoo]
+    g = Grammar.from_components(components=parse.rule_types, gram=2)
     fe = ConvFeatureExtractor(n_features=1000,
                               n_color_channels=1,
                               n_conv_channels=12,
                               bitmap_n_rows=128,
                               bitmap_n_cols=128)
-    parse = lambda s: to_learner_ast(parse_lsystem_to_ast(s))
+
     lg = LearnedGrammar(feature_extractor=fe, grammar=g,
-                        evaluator=evaluate, parser=parse,
+                        evaluator=eval_lsys, parser=parse_str_to_tuple,
                         start_symbol="LSystem")
     dataset = LSystemDataset.from_files(train_filenames)
-    train_loader = data.DataLoader(dataset)
+    train_loader = Tdata.DataLoader(dataset, num_workers=8)
     trainer = L.Trainer()  # limit_train_batches=100, max_epochs=1)
     trainer.fit(model=lg, train_dataloaders=train_loader)
 
@@ -148,9 +99,9 @@ if __name__ == "__main__":
     print("Trained grammar")
     print(lg.grammar)
 
-    test_loader = data.DataLoader(LSystemDataset.from_files(test_filenames))
+    test_loader = Tdata.DataLoader(LSystemDataset.from_files(test_filenames))
     for (s,), out in test_loader:
-        ast = parse(s)
+        ttree = parse_str_to_tuple(s)
         print(f"{s}\n"
-              f"  trained loss: {-lg.grammar.log_probability(lg.start_symbol, ast)}\n"
-              f"  untrained loss: {-lg.original_grammar.log_probability(lg.start_symbol, ast)}")
+              f"  trained loss: {-lg.grammar.log_probability(lg.start_symbol, ttree)}\n"
+              f"  untrained loss: {-lg.original_grammar.log_probability(lg.start_symbol, ttree)}")
--- src/parse.py
+++ src/parse.py
@@ -4,8 +4,8 @@ from typing import *
 import lark
 import eggy
 import numpy as np
+from lindenmayer import S0LSystem
 
-from cfg import CFG
 
 # grammar over lsystems
 lsystem_metagrammar = r"""
@@ -28,82 +28,81 @@ lsystem_metagrammar = r"""
     %ignore WS
 """
 
-lsystem_meta_cfg = CFG("LSystem", {
-    "LSystem": ["Axiom ; Rules"],
-    "Axiom": ["Symbols"],
-    "Symbols": ["Symbol Symbols", "Symbol"],
-    "Symbol": ["[ Symbols ]", "Nonterm", "Term"],
-    "Rules": ["Rule , Rules", "Rule"],
-    "Rule": ["Nonterm ~ Symbols"],
-    "Nonterm": ["F", "f"],
-    "Term": ["+", "-"]
-})
-
-# declare rule names
-_rule_names = {
-    "LSystem": ["lsystem"],
-    "Axiom": ["axiom"],
-    "Symbols": ["symbols", "symbol"],
-    "Symbol": ["bracket", "nonterm", "term"],
-    "Rules": ["rules", "rule"],
-    "Rule": ["arrow"],
-    "Nonterm": ["F", "f"],
-    "Term": ["+", "-"]
+rule_types = {
+    "lsystem": ["Axiom", "Rules", "LSystem"],
+    "axiom": ["Symbols", "Axiom"],
+    "symbols": ["Symbol", "Symbols", "Symbols"],
+    "symbol": ["Symbol", "Symbols"],
+    "bracket": ["Symbols", "Symbol"],
+    "nonterm": ["Nonterm", "Symbol"],
+    "term": ["Term", "Symbol"],
+    "rules": ["Rule", "Rules", "Rules"],
+    "rule": ["Rule", "Rules"],
+    "arrow": ["Nonterm", "Symbols", "Rule"],
+    "F": ["Nonterm"],
+    "f": ["Nonterm"],
+    "+": ["Term"],
+    "-": ["Term"],
 }
 
-# build translation dicts from rule name decls
-_d_name_to_nti = {
-    name: (nt, i)
-    for nt, names in _rule_names.items()
-    for i, name in enumerate(names)
+rule_str_semantics = {
+    "lsystem": lambda ax, rs: f"{ax};{rs}",
+    "axiom": lambda xs: xs,
+    "symbols": lambda x, xs: f"{x}{xs}",
+    "symbol": lambda x: x,
+    "bracket": lambda xs: f"[{xs}]",
+    "nonterm": lambda nt: nt,
+    "term": lambda t: t,
+    "rules": lambda r, rs: f"{r},{rs}",
+    "rule": lambda r: r,
+    "arrow": lambda nt, xs: f"{nt}~{xs}",
+    "F": lambda: "F",
+    "f": lambda: "f",
+    "+": lambda: "+",
+    "-": lambda: "-",
 }
-_d_nti_to_name = {v: k for k, v in _d_name_to_nti.items()}
+
+parser = lark.Lark(lsystem_metagrammar, start='lsystem', parser='lalr')
 
 
-def name_to_nti(s: str) -> Tuple[str, int]:
-    """Given the name of the i-th rule of nonterminal NT, return (NT, i)"""
-    return _d_name_to_nti[s]
+def parse_lsys_as_ltree(s: str) -> lark.Tree:
+    return parser.parse(s)
 
 
-def nti_to_name(s: str, i: int) -> str:
-    """Given (NT, i), return the name of the i-th rule of nonterminal NT"""
-    return _d_nti_to_name[s, i]
-
-
-def tree_to_tuple(node: lark.Tree | lark.Token):
+def ltree_to_ttree(node: lark.Tree | lark.Token):
     """Convert a lark tree into a nested tuple of strings"""
     if isinstance(node, lark.Tree):
-        return tuple([node.data] + [tree_to_tuple(x) for x in node.children])
+        return tuple([node.data] + [ltree_to_ttree(x) for x in node.children])
     else:
         return node.value
 
 
-def tree_to_sexp(node: lark.Tree | lark.Token) -> str:
+def ltree_to_sexp(node: lark.Tree | lark.Token) -> str:
     """Convert a lark tree into an s-expression string"""
     if isinstance(node, lark.Tree):
-        args = [node.data] + [tree_to_sexp(x) for x in node.children]
+        args = [node.data] + [ltree_to_sexp(x) for x in node.children]
         return f"({' '.join(args)})"
     else:
         return node.value
 
 
 def unigram_counts(tree: lark.Tree) -> Dict[str, np.ndarray]:
-    mg = lsystem_meta_cfg  # TODO: take this as an argument
-    counts: Dict[str, np.ndarray] = {nt: np.zeros(len(mg.rules[nt]))
-                                     for nt in mg.nonterminals}
-
-    def traverse(node: lark.Tree | lark.Token):
-        if isinstance(node, lark.Tree):
-            nt, i = name_to_nti(node.data)
-            counts[nt][i] += 1
-            for c in node.children:
-                traverse(c)
-        else:
-            nt, i = name_to_nti(node.value)
-            counts[nt][i] += 1
-
-    traverse(tree)
-    return counts
+    raise NotImplementedError
+    # counts: Dict[str, np.ndarray] = {nt: np.zeros(len(mg.rules[nt]))
+    #                                  for nt in mg.nonterminals}
+    #
+    # def traverse(node: lark.Tree | lark.Token):
+    #     if isinstance(node, lark.Tree):
+    #         nt, i = name_to_nti(node.data)
+    #         counts[nt][i] += 1
+    #         for c in node.children:
+    #             traverse(c)
+    #     else:
+    #         nt, i = name_to_nti(node.value)
+    #         counts[nt][i] += 1
+    #
+    # traverse(tree)
+    # return counts
 
 
 def bigram_counts(tree: lark.Tree) -> Dict[str, Dict[str, int]]:
@@ -125,7 +124,7 @@ def group_parens(tokens: List[str]) -> Dict[int, int]:
     return ends
 
 
-def tokens_to_sexp(tokens: List[str]) -> Tuple:
+def sexp_to_ttree(tokens: List[str]) -> Tuple:
     ends = group_parens(tokens)
 
     def to_ast_h(i: int, j: int) -> Tuple:
@@ -144,41 +143,41 @@ def tokens_to_sexp(tokens: List[str]) -> Tuple:
     return to_ast_h(1, len(tokens) - 1)
 
 
-def map_sexp(sexp: Tuple, f: Callable) -> Tuple:
-    if isinstance(sexp, tuple):
-        val, *children = sexp
+def eval_ttree_as_str(ttree: Tuple) -> str:
+    if isinstance(ttree, tuple):
+        symbol, *args = ttree
     else:
-        val, children = sexp, []
-    return tuple([f(val)] + [map_sexp(child, f) for child in children])
+        symbol, args = ttree, []
+    str_args = [eval_ttree_as_str(arg) for arg in args]
+    return rule_str_semantics[symbol](*str_args)
 
 
 if __name__ == '__main__':
     strs = [ 
-        # "F;F~F",
-        # "F;F~+-+--+++--F",
-        # "F;F~-+F+-",
-        # "F;F~[F]F",
-        # "F;F~[FF]FF",
-        # "F;F~[+F-F]+F-F",
+        "F;F~F",
+        "F;F~+-+--+++--F",
+        "F;F~-+F+-",
+        "F;F~[F]F",
+        "F;F~[FF]FF",
+        "F;F~[+F-F]+F-F",
         "F;F~[F]",
         "F;F~[FF+FF]",
-        # "F;F~F,F~F,F~F",
-        # "F;F~F,F~+-F,F~F",
-        # "F;F~F,F~+F-",
-        # "F;F~F,F~+F-,F~F",
-        # "F;F~F,F~FF,F~F,F~FF",
-        # "F;F~F[+F]F,F~F,F~F[+F]F",
+        "F;F~F,F~F,F~F",
+        "F;F~F,F~+-F,F~F",
+        "F;F~F,F~+F-",
+        "F;F~F,F~+F-,F~F",
+        "F;F~F,F~FF,F~F,F~FF",
+        "F;F~F[+F]F,F~F,F~F[+F]F",
     ]
-    parser = lark.Lark(lsystem_metagrammar, start='lsystem', parser='lalr')
     for s in strs:
-        ast = parser.parse(s)
+        ast = parse_lsys_as_ltree(s)
         # print(ast.pretty())
-        sexp = tree_to_sexp(ast)
+        sexp = ltree_to_sexp(ast)
         simplified = eggy.simplify(sexp)
-        print(f"{s} ->\n"
-              f"  {sexp} ->\n"
-              f"  {simplified}")
+        # print(f"{s} ->\n"
+        #       f"  {sexp} ->\n"
+        #       f"  {simplified}")
 
-        # tokens = tokenize(simplified)
-        # sexp = tokens_to_sexp(tokens)
-        # print(sexp)
+        ttree = sexp_to_ttree(tokenize(simplified))
+        s_simpl = eval_ttree_as_str(ttree)
+        print(f"{s} => {s_simpl}")
