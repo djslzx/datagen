7981a9f9092f14239a0130b00b47c4cd96f1c663
David J. Lee <djslzx@gmail.com>
2023-02-08 01:42:14 -0500

    parallel random baseline

--- src/random_baseline.py
+++ src/random_baseline.py
@@ -4,19 +4,25 @@ Randomly sample programs from a uniform PCFG over L-systems
 
 from __future__ import annotations
 from typing import *
+import multiprocessing as mp
 from grammar import Grammar
 import parse
 
-
-def random_sample(n_samples: int) -> Generator[str]:
-    g = Grammar.from_components(components=parse.rule_types, gram=2)
-    g.normalize_()
-    for _ in range(n_samples):
-        ttree = g.sample("LSystem")
-        s = parse.eval_ttree_as_str(ttree)
-        yield s
+# init metagrammar
+MG = Grammar.from_components(components=parse.rule_types, gram=2)
+MG.normalize_()
 
 
-if __name__ == "__main__":
-    for x in random_sample(100):
-        print(x)
+def sample_mg(i: int):
+    ttree = MG.sample("LSystem")
+    s = parse.eval_ttree_as_str(ttree)
+    print(f"{i}: {s}")
+    return s
+
+
+if __name__ == '__main__':
+    N_SAMPLES = 1000
+    FILE = "randoms.txt"
+    with mp.Pool(16) as pool, open(FILE, 'w') as f:
+        for x in pool.imap(sample_mg, range(N_SAMPLES)):
+            f.write(x + "\n")
