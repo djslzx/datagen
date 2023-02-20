2c81927c52bbfaf47f6a7142e2d9f3174768ca16
David J. Lee <djslzx@gmail.com>
2023-02-20 14:06:10 -0500

    version datasets

--- src/main.py
+++ src/main.py
@@ -16,12 +16,12 @@ def summary_stats(arr: np.ndarray) -> str:
 
 def make_df(names: List[str], in_files: List[str], out_files: List[str]) -> pd.DataFrame:
     rows = []
-    for name, in_file, out_file in zip(names, in_files, out_files):
+    for (name, version), in_file, out_file in zip(names, in_files, out_files):
         for in_line, out_line in zip(*read_files(in_file, out_file)):
             x, y = len(in_line), len(out_line)
-            entry = (in_line, out_line, x, y, x-y, name)
+            entry = (in_line, out_line, x, y, x-y, f"{name}-{version}", name, version)
             rows.append(entry)
-    df = pd.DataFrame(rows, columns=["in", "out", "in length", "out length", "diff", "source"])
+    df = pd.DataFrame(rows, columns=["in", "out", "in length", "out length", "diff", "id", "dataset", "version"])
     return df
 
 
@@ -33,10 +33,10 @@ def pluck_egg_examples(df: pd.DataFrame, k=10):
             s += f"{row['in']} =>\n  {row['out']}, {row['diff']} tokens reduced\n"
         return s
 
-    for source in df["source"].unique():
+    for source in df["id"].unique():
         fdf = df.loc[(df["diff"] > 0) &
                      (df["out length"] > 0) &
-                     (df["source"] == source)].sort_values(by="diff", ascending=False)
+                     (df["id"] == source)].sort_values(by="diff", ascending=False)
         print(f"Filtered {source}: {(df['source'] == source).size} => {fdf.size}")
         m = len(fdf)//2
         print("Max:\n", view(fdf.head(k)))
@@ -46,21 +46,20 @@ def pluck_egg_examples(df: pd.DataFrame, k=10):
 
 
 def plot_egg_scatter(df: pd.DataFrame) -> plt.Axes:
-    counts = df.groupby(["in length", "out length", "source"]).size().reset_index(name="count")
+    counts = df.groupby(["in length", "out length", "dataset", "version"]).size().reset_index(name="count")
     p = sns.relplot(data=counts, x="in length", y="out length",
-                    hue="source", col="source", size="count", sizes=(10, 1000))
+                    col="dataset", row="version", size="count", sizes=(10, 1000))
     x_max = df["in length"].max()
     p.map(lambda *args, **kwargs: plt.plot([0, x_max], [0, x_max], color="gray"))
     return p
 
 
 def plot_egg_cdf(df: pd.DataFrame) -> plt.Axes:
-    return sns.displot(df, x="diff", hue="source", kind="ecdf")
+    return sns.displot(df, x="diff", col="dataset", hue="version", kind="ecdf")
 
 
 def plot_egg_pdf(df: pd.DataFrame) -> plt.Axes:
-    return sns.displot(df, x="diff", hue="source", kind="kde", bw_adjust=.5)
-    # return sns.displot(df, x="diff", hue="source", multiple="dodge", stat="density", common_norm=False)
+    return sns.displot(df, x="diff", col="dataset", hue="version", kind="kde", bw_adjust=.5)
 
 
 def read_files(in_file: str, out_file: str) -> Tuple[List[str], List[str]]:
@@ -112,12 +111,15 @@ def simplify_file(in_path: str, out_path: str, score_thresh=None):
 def plot():
     sns.set_theme()
     test_paths = [
-        "ns-simpl", "../datasets/ns/ns.txt", "../datasets/ns/ns-simpl.txt",
-        "ns-filt", "../datasets/ns/ns.txt", "../datasets/ns/ns-filt.txt",
-        "ns-simpl2", "../datasets/ns/ns.txt", "../datasets/ns/ns-simpl2.txt",
-        "ns-filt2", "../datasets/ns/ns.txt", "../datasets/ns/ns-filt2.txt",
-        # "random", "../datasets/random/random_100k.txt", "../datasets/random/random_100k_simpl.txt",
-        # "random2", "../datasets/random/random_100k.txt", "../datasets/random/random_100k_simpl2.txt",
+        ("ns-simpl", "1"), "../datasets/ns/ns.txt", "../datasets/ns/ns-simpl.txt",
+        ("ns-filt", "1"), "../datasets/ns/ns.txt", "../datasets/ns/ns-filt.txt",
+        ("ns-simpl", "2"), "../datasets/ns/ns.txt", "../datasets/ns/ns-simpl2.txt",
+        ("ns-filt", "2"), "../datasets/ns/ns.txt", "../datasets/ns/ns-filt2.txt",
+        ("ns-simpl", "3"), "../datasets/ns/ns.txt", "../datasets/ns/ns-simpl3.txt",
+        ("ns-filt", "3"), "../datasets/ns/ns.txt", "../datasets/ns/ns-filt3.txt",
+        ("random", "1"), "../datasets/random/random_100k.txt", "../datasets/random/random_100k_simpl.txt",
+        ("random", "2"), "../datasets/random/random_100k.txt", "../datasets/random/random_100k_simpl2.txt",
+        ("random", "3"), "../datasets/random/random_100k.txt", "../datasets/random/random_100k_simpl3.txt",
     ]
 
     df = make_df(test_paths[::3], test_paths[1::3], test_paths[2::3])
@@ -125,16 +127,16 @@ def plot():
     print(df)
     # print(filtered_df)
 
-    # plot_egg_scatter(filtered_df)
-    plot_egg_cdf(df)
-    # plot_egg_pdf(filtered_df)
+    plot_egg_scatter(df)
+    # plot_egg_cdf(df)
+    # plot_egg_pdf(df)
     plt.show()
     # pluck_egg_examples(df, k=3)
 
 
 if __name__ == "__main__":
-    simplify_file("../datasets/ns/ns.txt", "../datasets/ns/ns-simpl2.txt")
-    simplify_file("../datasets/ns/ns.txt", "../datasets/ns/ns-filt2.txt", 0.001)
-    # simplify_file("../datasets/random/random_100k.txt", "../datasets/random/random_100k_simpl2.txt")
+    # simplify_file("../datasets/ns/ns.txt", "../datasets/ns/ns-simpl3.txt")
+    # simplify_file("../datasets/ns/ns.txt", "../datasets/ns/ns-filt3.txt", 0.001)
+    simplify_file("../datasets/random/random_100k.txt", "../datasets/random/random_100k_simpl3.txt")
     plot()
 
