3085abf4022aee1b65761487bea4c1d1e14cfc5c
David J. Lee <djslzx@gmail.com>
2023-02-24 00:59:19 -0500

    use Timing to clean up timing; fix up evo to work with changes

--- src/evo.py
+++ src/evo.py
@@ -2,6 +2,8 @@
 Test out evolutionary search algorithms for data augmentation.
 """
 import pdb
+import sys
+
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
 from typing import *
@@ -16,18 +18,7 @@ from zoo import zoo
 from lindenmayer import S0LSystem
 from featurizers import ResnetFeaturizer, Featurizer, RawFeaturizer
 import parse
-
-# Set up file paths
-PCFG_CACHE_PREFIX = ".cache/"
-
-for directory in [".cache/", ".cache/imgs/"]:
-    try:
-        open(directory, "r")
-    except FileNotFoundError:
-        print(f"{directory} directory not found, making dir...")
-        mkdir(directory)
-    except IsADirectoryError:
-        pass
+from util import Timing
 
 # Hyper-parameters
 DRAW_ARGS = {
@@ -38,9 +29,10 @@ DRAW_ARGS = {
 }
 ROLLOUT_DEPTH = 3
 START = "LSystem"
+OUTDIR = "../out/evo"
 
 
-def next_gen(metagrammar: Grammar, n_next_gen: int, p_arkv: float, sentence_limit: int) -> Tuple[np.ndarray, Set]:
+def next_gen(metagrammar: Grammar, n_next_gen: int, p_arkv: float) -> Tuple[np.ndarray, Set]:
     popn = np.empty(n_next_gen, dtype=object)
     arkv = set()
     for i in range(n_next_gen):  # parallel
@@ -57,33 +49,30 @@ def semantic_score(prev_gen: np.ndarray, new_gen: np.ndarray,
     n_features = featurizer.n_features
 
     # build knn data structure
-    print("Featurizing popn...")
-    t_feat = time.time()
-    popn_features = np.empty((len(prev_gen), n_features * n_samples))
-    for i, s in enumerate(prev_gen):  # parallel
-        sys = S0LSystem.from_sentence(list(s))
-        for j in range(n_samples):
-            bmp = sys.draw(sys.nth_expansion(ROLLOUT_DEPTH), **DRAW_ARGS)
-            popn_features[i, j * n_features: (j + 1) * n_features] = featurizer.apply(bmp)
-    t_knn = time.time()
-    print(f"Featurizing popn took {t_knn - t_feat}s")
-    knn = NearestNeighbors(n_neighbors=min(n_neighbors, len(prev_gen))).fit(popn_features)
-    print(f"Building knn data structure took {time.time() - t_knn}s")
+    with Timing("Computing features"):
+        popn_features = np.empty((len(prev_gen), n_features * n_samples))
+        for i, s in enumerate(prev_gen):  # parallel
+            sys = S0LSystem.from_sentence(list(s))
+            for j in range(n_samples):
+                bmp = sys.draw(sys.nth_expansion(ROLLOUT_DEPTH), **DRAW_ARGS)
+                popn_features[i, j * n_features: (j + 1) * n_features] = featurizer.apply(bmp)
+
+    with Timing("Building knn data structure"):
+        knn = NearestNeighbors(n_neighbors=min(n_neighbors, len(prev_gen))).fit(popn_features)
 
     # compute scores of next generation
-    print("Scoring instances...")
-    t_scoring = time.time()
-    scores = np.empty(len(new_gen))
-    for i, s in enumerate(new_gen):  # parallel
-        features = np.empty((1, n_features * n_samples))
-        sys = S0LSystem.from_sentence(list(s))
-        for j in range(n_samples):
-            bmp = sys.draw(sys.nth_expansion(ROLLOUT_DEPTH), **DRAW_ARGS)
-            features[0, j * n_features: (j + 1) * n_features] = featurizer.apply(bmp)
-        distances, indices = knn.kneighbors(features)
-        scores[i] = distances.sum(axis=1).item()
-        # scores[i] = distances.mean(axis=1).item() ** 2 / len(s)  # prioritize shorter agents
-    print(f"Scoring took {time.time() - t_scoring}s")
+    with Timing("Scoring instances"):
+        scores = np.empty(len(new_gen))
+        for i, s in enumerate(new_gen):  # parallel
+            features = np.empty((1, n_features * n_samples))
+            sys = S0LSystem.from_sentence(list(s))
+            for j in range(n_samples):
+                bmp = sys.draw(sys.nth_expansion(ROLLOUT_DEPTH), **DRAW_ARGS)
+                features[0, j * n_features: (j + 1) * n_features] = featurizer.apply(bmp)
+            distances, indices = knn.kneighbors(features)
+            scores[i] = distances.sum(axis=1).item()
+            # scores[i] = distances.mean(axis=1).item() ** 2 / len(s)  # prioritize shorter agents
+
     return scores
 
 
@@ -105,12 +94,10 @@ def novelty_search(init_popn: List[str],
                    n_neighbors: int,
                    next_gen_ratio: int,
                    sentence_limit: int,
-                   measure_novelty_within_generation: bool = False,
-                   name: str = "") -> Set[Iterable[str]]:
+                   measure_novelty_within_generation: bool,
+                   out_dir: str) -> Set[Iterable[str]]:
     # todo: use tensorboard via lightning?
-
-    logparams = locals()
-    del logparams['init_popn']
+    log_params = locals()  # Pull local variables so we can log the args that were passed to this function
 
     arkv = set()
     popn = np.array(init_popn, dtype=object)
@@ -122,56 +109,41 @@ def novelty_search(init_popn: List[str],
         t_start = time.time()
 
         # generate next gen
-        print("Fitting metagrammar...")
-        t_io = time.time()
-        corpus = [parse.parse_lsys(x) for x in popn]
-        counts = parse.multi_count_bigram(corpus)
-        metagrammar.from_bigram_counts_(counts)
-        print(f"Fitting took {time.time() - t_io}s.")
-
-        print("Generating next gen...")
-        t_gen = time.time()
-        new_gen, new_arkv = next_gen(metagrammar=metagrammar, n_next_gen=n_next_gen,
-                                     p_arkv=p_arkv, sentence_limit=sentence_limit)
-        arkv |= new_arkv
-        print(f"Generating took {time.time() - t_gen}s.")
+        with Timing("Fitting metagrammar"):
+            corpus = [parse.parse_lsys(x) for x in popn]
+            counts = parse.multi_count_bigram(corpus)
+            metagrammar.from_bigram_counts_(counts)
+        with Timing("Generating next gen"):
+            new_gen, new_arkv = next_gen(metagrammar=metagrammar, n_next_gen=n_next_gen, p_arkv=p_arkv)
+            arkv |= new_arkv
 
         # compute popn features, build knn data structure, and score next_gen
-        print("Scoring population...")
-        t_score = time.time()
-        if measure_novelty_within_generation:
-            prev_gen = new_gen
-        else:
-            prev_gen = np.concatenate((np.array(popn), np.array(list(arkv), dtype=object)), axis=0)
-        scores = semantic_score(prev_gen=prev_gen,
-                                new_gen=new_gen,
-                                featurizer=featurizer,
-                                n_neighbors=n_neighbors,
-                                n_samples=n_samples)
-        # print(f"Semantic scoring took {time.time() - t_score}s.")
-        # scores = syntactic_semantic_score(popn=next_gen, semantic_scores=sem_scores)  # fixme
-        print(f"Scoring took {time.time() - t_score}s.")
+        with Timing("Scoring population"):
+            if measure_novelty_within_generation:
+                prev_gen = new_gen
+            else:
+                prev_gen = np.concatenate((np.array(popn), np.array(list(arkv), dtype=object)), axis=0)
+            scores = semantic_score(prev_gen=prev_gen,
+                                    new_gen=new_gen,
+                                    featurizer=featurizer,
+                                    n_neighbors=n_neighbors,
+                                    n_samples=n_samples)
 
         # cull popn
-        print("Culling popn...")
-        t_cull = time.time()
-        indices = np.argsort(-scores)  # sort descending: higher mean distances first
-        new_gen = new_gen[indices]
-        popn = new_gen[:max_popn_size]  # take indices of top `max_popn_size` agents
-        print(f"Culling took {time.time() - t_cull}s.")
+        with Timing("Culling popn"):
+            indices = np.argsort(-scores)  # sort descending: higher mean distances first
+            new_gen = new_gen[indices]
+            popn = new_gen[:max_popn_size]  # take indices of top `max_popn_size` agents
 
         # make labels
-        print("Logging...")
-        t_log = time.time()
-        scores = scores[indices]
-        min_score = scores[max_popn_size - 1]
-        labels = [f"{score:.2e}" + ("*" if score >= min_score else "")
-                  for score in scores]
-        t_taken = time.time() - t_start
-        print(f"Logging took {time.time() - t_log}s.")
+        with Timing("Logging"):
+            scores = scores[indices]
+            min_score = scores[max_popn_size - 1]
+            labels = [f"{score:.2e}" + ("*" if score >= min_score else "")
+                      for score in scores]
 
         print("====================")
-        print(f"Completed iteration {iter} in {t_taken:.2f}s.")
+        print(f"Completed iteration {iter} in {time.time() - t_start:.2f}s.")
         print(f"New generation ({n_next_gen}):")
         for agent, label in zip(new_gen, labels):
             print(f"  {''.join(agent)} - {label}")
@@ -182,13 +154,13 @@ def novelty_search(init_popn: List[str],
         print("====================")
 
         # save gen
-        with open(f"{PCFG_CACHE_PREFIX}{name}-gen-{iter}.txt", "w") as f:
-            f.write(f"# {logparams}\n")
+        with open(f"{out_dir}/gen-{iter}.txt", "w") as f:
+            f.write(f"# {log_params}\n")
             for agent, label in zip(new_gen, labels):
                 f.write("".join(agent) + f" : {label}\n")
 
         # save arkv
-        with open(f"{PCFG_CACHE_PREFIX}{name}-arkv.txt", "a") as f:
+        with open(f"{out_dir}/arkv.txt", "a") as f:
             for agent in new_arkv:
                 f.write(''.join(agent) + "\n")
 
@@ -209,8 +181,19 @@ def main(name: str):
     for i, args in enumerate(it.product(popn_sizes, arkv_growth_rates, iterations,
                                         neighborhood_sizes, novelty_within_gen)):
         popn_size, arkv_growth_rate, iters, neighborhood_size, novelty_within = args
+        out_dir = f"{OUTDIR}/{t}-{name}-{i}"
+
+        try:
+            f = open(out_dir, "r")
+            f.close()
+        except FileNotFoundError:
+            print(f"{out_dir} directory not found, making dir...", file=sys.stderr)
+            mkdir(out_dir)
+        except IsADirectoryError:
+            pass
+
         params = {
-            'name': f"{t}-{name}-{i}",
+            'out_dir': out_dir,
             'init_popn': seed_popn,
             'iters': iters,
             'featurizer': ResnetFeaturizer(disable_last_layer=False,
