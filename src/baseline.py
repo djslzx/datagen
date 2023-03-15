c99be4f61ff8bc0b32e95f864dc451d2de90c6f4
David J. Lee <djslzx@gmail.com>
2023-03-15 00:10:06 -0400

    regex baseline

--- src/baseline.py
+++ src/baseline.py
@@ -6,24 +6,23 @@ from __future__ import annotations
 from typing import *
 import multiprocessing as mp
 
+from lang import Language
 from lindenmayer import LSys, NilError
+from regexpr import Regex
 from zoo import zoo_strs
 
-# init metagrammar
+LEN_CAP = 100
+
+# init lsystem metagrammar
 lsys = LSys(45, 3, 3, 128, 128)
 lsys_fitted = LSys(45, 3, 3, 128, 128)
 lsys_fitted.fit([lsys_fitted.parse(s) for s in zoo_strs], alpha=0.01)
-LEN_CAP = 100
+
+# init regex metagrammar
+rgx = Regex()
 
 
-def sample(i) -> str:
-    t = lsys.sample()
-    s = lsys.to_str(t)
-    print(s)
-    return s
-
-
-def sample_fitted_capped(i) -> str:
+def sample_lsys(i) -> str:
     while True:
         t = lsys_fitted.sample()
         if len(t) <= LEN_CAP:
@@ -33,7 +32,7 @@ def sample_fitted_capped(i) -> str:
     return s
 
 
-def sample_simplify(i) -> Optional[str]:
+def sample_simplified_lsys(i) -> Optional[str]:
     s = lsys.sample()
     try:
         s = lsys.simplify(s)
@@ -43,13 +42,23 @@ def sample_simplify(i) -> Optional[str]:
         return None
 
 
-def take_samples_to_file(n_samples: int, out_file: str):
+def sample_regex(i) -> str:
+    while True:
+        t = rgx.sample()
+        if len(t) <= LEN_CAP:
+            break
+    s = rgx.to_str(t)
+    print(s)
+    return s
+
+
+def sample_to_file(sampler: Callable, n_samples: int, out_file: str):
     with mp.Pool(16) as pool, open(out_file, 'w') as f:
-        for x in pool.imap(sample_fitted_capped, range(n_samples)):
+        for x in pool.imap(sampler, range(n_samples)):
             f.write(x + "\n")
 
 
 if __name__ == '__main__':
     N_SAMPLES = 100_000
-    FILE = "../datasets/random/fitted_100k.txt"
-    take_samples_to_file(N_SAMPLES, FILE)
+    FILE = "../datasets/regex/random/random_100k.txt"
+    sample_to_file(sample_regex, N_SAMPLES, FILE)
