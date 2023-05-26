34faac28812465b259d402da113c4228b57aa1ce
David J. Lee <djslzx@gmail.com>
2023-05-26 16:35:20 -0400

    swap shape axes

--- src/util.py
+++ src/util.py
@@ -210,8 +210,8 @@ def plot(imgs: List[np.array],
     if isinstance(imgs[0], np.ndarray) and imgs[0].dtype != float:
         imgs = [img.astype(float) for img in imgs]
 
-    fig, ax = plt.subplots(*shape, figsize=(1.28 * shape[0] + 0.5,
-                                            1.28 * shape[1] + 0.5))
+    fig, ax = plt.subplots(*shape, figsize=(1.28 * shape[1] + 0.5,
+                                            1.28 * shape[0] + 0.5))
     if shape == (1, 1):
         ax.imshow(imgs[0][0])
         if labels is not None:
