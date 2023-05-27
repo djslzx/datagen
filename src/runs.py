5fffa461213c57c53ebcffaf37ce3fd09116cce8
David J. Lee <djslzx@gmail.com>
2023-05-26 23:16:09 -0400

    fix runs.py render sorting

--- src/runs.py
+++ src/runs.py
@@ -30,24 +30,23 @@ def make_df(sweep_path: str):
     return pd.DataFrame(records)
 
 
-def render_run(run_id: str):
-    df = pd.read_csv(f"../sweeps/{run_id}/{run_id}.csv")
-    l = LSys(theta=45, step_length=4, render_depth=3, n_rows=128, n_cols=128, kind="deterministic")
+def render_run(prefix:str, run_id: str):
+    df = pd.read_csv(f"{prefix}/{run_id}/{run_id}.csv")
+    l = LSys(step_length=4, render_depth=3, n_rows=128, n_cols=128, kind="deterministic")
     # plot rows in batches by generation
     # step program kind dist length score chosen
     steps = df.step.unique()
     print(run_id)
     for step in steps[::10]:
         print(f"  step: {step}")
-        gen = df.loc[df.step == step][:100]
-        gen.sort_values(by='score', ascending=False, inplace=True)
+        gen = df.loc[(df.step == step) & (df.chosen == True)].sort_values(by='score', ascending=False)[:100]
         imgs = [l.eval(l.parse(x.program)) for i, x in gen.iterrows()]
         labels = [f"{x.score:.3e}{'*' if x.chosen else ''}" for i, x in gen.iterrows()]
         util.plot(imgs, shape=(10, 10),
                   # labels=labels,
                   title=f"{run_id} step={step}",
                   fontsize=3,
-                  saveto=f"../sweeps/{run_id}/step{step}.png")
+                  saveto=f"{prefix}/{run_id}/step{step}.png")
 
 
 def eval_run(run_id: str):
