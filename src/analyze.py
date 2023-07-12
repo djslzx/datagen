21e81484af65fe8a75f35216e9f3765449e45aad
David J. Lee <djslzx@gmail.com>
2023-07-12 14:52:38 -0400

    save embeddings using numpy; batch feature extraction; hist

--- src/analyze.py
+++ src/analyze.py
@@ -12,27 +12,30 @@ import util
 tqdm.pandas()
 
 
-def embed_instructions(df: pd.DataFrame, ft: feat.Featurizer, outdir: str):
+def embed_instructions(df: pd.DataFrame, ft: feat.Featurizer, outdir: str, batch_size=10):
     """
     Embed instructions using the given featurizer
     """
     print("Embedding instructions...", file=sys.stderr)
-    df["embedding"] = df["instruction"].progress_apply(ft.apply)
-    df.to_csv(f"{outdir}/embeddings.csv", index=False)
-    return df
+    all_embeddings = []
+    for batch in tqdm(util.batched(df["instruction"], batch_size), total=len(df)//batch_size):
+        embeddings = ft.apply(batch)
+        all_embeddings.extend(embeddings)
+    np_embeddings = np.array(all_embeddings)
+    np.save(f"{outdir}/embeddings.npy", np_embeddings)
+    return np_embeddings
 
-def write_dists(df: pd.DataFrame, outdir: str):
+def write_dists(embeddings: np.ndarray, outdir: str):
     """
     Compute pairwise distances between embeddings of rows
     """
     print("Computing distances...", file=sys.stderr)
     rows = []
-    for i, r1 in tqdm(df.iterrows(), total=len(df)):
-        for j, r2 in df.iterrows():
-            if i < j:
-                rows.append({"i": i, "j": j, "dist": np.linalg.norm(r1["embedding"] - r2["embedding"])})
+    for i, e1 in tqdm(enumerate(embeddings), total=len(embeddings)):
+        for j, e2 in enumerate(embeddings[i+1:]):
+            rows.append({"i": i, "j": j, "dist": np.linalg.norm(e1 - e2)})
     dists = pd.DataFrame(rows)
-    dists.to_csv(f"{outdir}/dists.csv", index=False)
+    dists.to_csv(f"{outdir}/dists.csv", index=False, float_format="%.4f")
     return dists
 
 
@@ -43,17 +46,17 @@ def dists_to_histogram(df: pd.DataFrame, outdir: str):
     plt.hist(df["dist"])
     plt.show()
 
+
 if __name__ == "__main__":
-    # with open("../datasets/evol_teacher_80k.json", "r") as f:
-    #     data = json.load(f)
-    #     df = pd.DataFrame(data)
+    with open("../datasets/evol_teacher_80k.json", "r") as f:
+        data = json.load(f)[:1000]
+        df = pd.DataFrame(data)
     ft = feat.SentenceFeaturizer()
 
-    with open("../out/dists/evol_teacher_80k/embeddings.csv", "r") as f:
-        embedding_data = json.load(f)
-
-    embedding_df = pd.DataFrame(embedding_data)
-    print(embedding_df).head()
-
     os.makedirs("../out/dists/evol_teacher_80k", exist_ok=True)
-    write_dists(embedding_df, "../out/dists/evol_teacher_80k/")
+    embed_instructions(df, ft, "../out/dists/evol_teacher_80k")
+    embeddings = np.load("../out/dists/evol_teacher_80k/embeddings.npy")
+
+    dists = write_dists(embeddings, "../out/dists/evol_teacher_80k")
+    plt.hist(dists["dist"])
+    plt.show()
