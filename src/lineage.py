322ae9b52645ca76d04ffd640f3bbb30fa847db7
David J. Lee <djslzx@gmail.com>
2023-08-24 14:55:42 -0400

    adjust to suit updated evolinstruct data

--- src/lineage.py
+++ src/lineage.py
@@ -47,7 +47,7 @@ def build_lineage_graph(df: pd.DataFrame, chat: ChatOpenAI = None) -> gt.Graph:
             if "sample.input name" in row:
                 parent_name = row["sample.input name"]
             elif chat:
-                parent_name = wizard.name_programming_problem(chat, parent_text)
+                parent_name = wizard.propose_name(chat, parent_text)
             else:
                 parent_name = ""
 
@@ -126,81 +126,112 @@ def load_json_as_df(filename: str) -> pd.DataFrame:
 
 def parent_embed_dist(df: pd.DataFrame, embeddings: np.ndarray) -> pd.DataFrame:
     """analyze semantic distances between parents and children"""
-    # prepend rows for starting dataset
-    orig_rows = pd.DataFrame([
-        {
-            "iteration": 0,
-            "score": None,
-            "rank": None,
-            "chosen?": True,
-            "archived?": False,
-            "sample.input": None,
-            "sample.evol_method": None,
-            "sample.output": x,
-        }
-        for x in df[df["iteration"] == 0]["sample.input"].unique()
-    ])
-    df["iteration"] += 1
-    df = pd.concat([orig_rows, df], ignore_index=True)
+    df.set_index("id", inplace=True)
+    df["parent"].fillna(df.index.to_series(), inplace=True)
+    df["parent"] = df["parent"].astype(np.int32)
+    df["embedding"] = pd.Series([v for v in embeddings], dtype=object)
+    assert type(df["embedding"].iloc[0]) == np.ndarray
 
-    # add embedding as column of df
-    df["sample.output.embedding"] = pd.Series([v for v in embeddings], dtype=object)
-    assert type(df["sample.output.embedding"].iloc[0]) == np.ndarray
+    # change type of score to numpy float32
+    if "score" in df.columns:
+        df["score"] = df["score"].astype(np.float32)
 
-    # set a row's sample input index by looking for the first row whose sample output is the same as the current row's sample input
-    # unless the row is a starting dataset row, in which case the sample input index is the row's index
-    df["sample.input.index"] = df.apply(
-        lambda row: row.name if row["iteration"] == 0 else df[df["sample.output"] == row["sample.input"]].index[0],
-        axis=1
-    )
-    # pull the embedding of the sample input from the input's row
-    df["sample.input.embedding"] = df.apply(
-        lambda row: df["sample.output.embedding"].iloc[row["sample.input.index"]],
-        axis=1
-    )
-    # take distance between input and output embeddings
-    df["parent-child dist"] = df.apply(
-        lambda row: np.linalg.norm(row["sample.input.embedding"] - row["sample.output.embedding"]),
-        axis=1
-    )
+    df["parent embedding"] = df.apply(lambda row: df.loc[row["parent"]]["embedding"], axis=1)
+    df["parent name"] = df.apply(lambda row: df.loc[row["parent"]]["name"], axis=1)
+    df["pc dist"] = df.apply(lambda row: np.linalg.norm(row["embedding"] - row["parent embedding"]), axis=1)
 
-    df["sample.evol_method"] = df["sample.evol_method"].apply(lambda x: wizard.EVOL_METHOD_NAMES[x] if x else None)
-
-    # drop embedding columns
-    df = df.drop(columns=["sample.output.embedding", "sample.input.embedding"])
+    df = df[df["iter"] > 0]
+    df = df.drop(columns=["embedding", "parent embedding"])
 
     return df
+
+
+def plot_by_generation(df: pd.DataFrame, y: str):
+    """plot a metric by generation"""
+    fig, axes = plt.subplots(1, 2, figsize=(12, 6))  # 1 row, 2 columns
+    sns.lineplot(df, x="iteration", y=y, ax=axes[1])
+    sns.lineplot(df, x="iteration", y=y, hue="sample.evol_method", ax=axes[0])
+    plt.suptitle(f"{y} by generation")
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == "__main__":
     pd.set_option("display.max_columns", None)
     pd.set_option("display.min_rows", 50)
 
-    # chat = ChatOpenAI(temperature=0.9)
-    # df = load_json_as_df("../datasets/code_alpaca_100_nov_100xAll.jsonl")
-    embeddings = np.load("../datasets/code_alpaca_tiny_nov_10x20_1-embeddings.npy")
-    df = load_json_as_df("../datasets/code_alpaca_tiny_nov_10x20_1.jsonl")
-    df = parent_embed_dist(df, embeddings)
-    print(df)
+    data = util.load_jsonl(f"../datasets/evol-instruct-single-100-2023-08-24T12:17:16.661029.jsonl")
+    # embeddings = wizard.embed([data["text"] for data in data], saveto="../datasets/evol-instruct-single-100-2023-08-24T12:17:16.661029.npy")
+    embeddings = np.load("../datasets/evol-instruct-single-100-2023-08-24T12:17:16.661029.npy")
 
-    sns.relplot(df, x="iteration", y="parent-child dist", hue="sample.evol_method", kind="line")
+    df = pd.DataFrame(data)
+    df = parent_embed_dist(df, embeddings)
+    df = df[df["iter"] > 0]
+
+    # pc dist by mutator
+    dists_by_mut = df.groupby("mutator")["pc dist"]
+    print(dists_by_mut.describe())
+    # plot using sns boxplot
+    sns.catplot(data=df, x="mutator", y="pc dist")
+    # increase fig size
+    plt.gcf().set_size_inches(12, 6)
     plt.show()
 
+    # input-output pair with greatest parent-child dist in each generation
+    # max_pc = df.loc[df.groupby("iteration")["parent-child dist"].idxmax()]
+    # print(max_pc[["iteration", "sample.input.name", "sample.output.name", "parent-child dist"]].to_markdown())
+    # min_pc = df.loc[df.groupby("iteration")["parent-child dist"].idxmin()]
+    # print(min_pc[["iteration", "sample.input.name", "sample.output.name", "parent-child dist"]].to_markdown())
+
+    # find outputs with "sorry"
+    # sorry = df[df["sample.output.text"].str.lower().str.contains(["sorry", "apolog", "can't", "unable", "unfortunately"])]
+    # print(sorry)
+
+    # # print 3 best parent-child pairs with greatest score in every 10th generation
+    # samples = (
+    #     df
+    #     .loc[df["iteration"] % 10 == 0]
+    #     .sort_values("score", ascending=False)
+    #     .groupby("iteration")
+    #     .head(1)
+    #     .sort_values("iteration")
+    #     [["iteration", "sample.output.text", "score", "parent-child dist", "sample.evol_method"]]
+    # )
+    # for i, row in samples.iterrows():
+    #     print(f"Generation {row['iteration']}, "
+    #           f"score: {row['score']}, "
+    #           f"evol_method: {row['sample.evol_method']}")
+    #     print("#+begin_quote")
+    #     print(row['sample.output.text'])
+    #     print("#+end_quote")
+
+    # plot_by_generation(df, "total len")
+    # plot_by_generation(df, "len")
+    # plot_by_generation(df, "num words")
+    # plot_by_generation(df, "avg word length")
+    # plot_by_generation(df, "parent-child dist")
+    # plot_by_generation(df, "scaled parent-child dist")
+    # plot_by_generation(df, "score")
+    # plot_by_generation(df, "rank")
+
+    # fig, axes = plt.subplots(2, 2, figsize=(12, 6))
+    # sns.lineplot(df, x="iteration", y="parent-child dist", ax=axes[0, 0])
+    # sns.lineplot(df, x="iteration", y="scaled parent-child dist", ax=axes[0, 1])
+    # sns.lineplot(df, x="iteration", y="parent-child dist", hue="sample.evol_method", ax=axes[1, 0])
+    # sns.lineplot(df, x="iteration", y="scaled parent-child dist", hue="sample.evol_method", ax=axes[1, 1])
+    # plt.suptitle("Parent-child distance by generation")
+    # plt.tight_layout()
+    # plt.show()
 
     # G = build_lineage_graph(df, chat=None)
-    # # root = add_root_to_sources_(G)
-    # # print(G)
+    # root = add_root_to_sources_(G)
+    # print(G)
     #
-    # embeddings = np.load("../datasets/code_alpaca_tiny_nov_10x20_1-embeddings.npy")
-    # # embeddings = np.load("../datasets/code_alpaca_100_nov_100xAll-embeddings.npy")
-    # print(len(embeddings))
-
     # vG = gt.GraphView(G,
     #                   vfilt=lambda v: G.vp.iteration[v] < 4,
     #                   efilt=lambda e: G.vp.iteration[e.target()] < 4)
     # draw_lineage_graph(
     #     G,
-    #     pos=genealogy_layout(G),
+    #     pos=genealogy_layout(G),  # gt.radial_tree_layout(vG, root=root)
     #     outfile="lineage_graph_big"
     # )
-    # draw_lineage_graph(vG, pos=gt.radial_tree_layout(vG, root=root))
