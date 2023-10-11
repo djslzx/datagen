babdc07e2994ec0ddf2162cb37312f658dd549fa
David Jinsoo Lee <djslzx@gmail.com>
2023-10-11 15:23:43 -0400

    split tests and run each test individually

--- src/runner.py
+++ src/runner.py
@@ -17,19 +17,16 @@ import util
 
 
 def make_programs(solns: List[str], tests: List[str]) -> Generator[str, None, None]:
-    n_tests = len(tests)
-    main = "\n".join([
-        "for test in [" + ", ".join([f"test_{i}_{j}"
-                                     for i in range(1, n_tests + 1)
-                                     for j in range(1, 6)]) +
-        "]:",
-        "    assert test(), f'{test.__name__} did not pass'", 
-    ])
     for soln in solns:
-        body = [t.replace("test_", f"test_{i}_")
-                for i, t in enumerate(tests, 1)]
-        program = "\n\n".join([soln, *body, main])
-        yield program
+        for test in tests:
+            test_names = find_tests(test)
+            if not test_names:
+                tester = "assert False, 'no tests found'"
+            else:
+                test_name = test_names[0]
+                tester = f"assert {test_name}, '{test_name} did not pass'"
+            program = "\n\n".join([soln, test, tester])
+            yield program
 
 
 def eval_dataset(filename: str, n_samples: int) -> Generator[Dict, None, None]:
@@ -41,14 +38,20 @@ def eval_dataset(filename: str, n_samples: int) -> Generator[Dict, None, None]:
 
     df = pd.read_csv(filename)
     indices = np.random.randint(low=0, high=len(df), size=n_samples)
+
     for i, row in df.iloc[indices].iterrows():
         solns = [row[f"soln-{i}"] for i in range(3)]
-        tests = [util.strip_markdown(row[f"tests(text, soln_{i})"]) for i in range(3)]
         solns = [x for x in solns if x and not isnan(x)]
-        tests = [x for x in tests if x and not isnan(x)]
-        programs = make_programs(solns, tests)
-        for program in programs:
+
+        tests = [util.strip_markdown(row[f"tests(text, soln_{i})"]) for i in range(3)]
+        tests = [test for x in tests
+                 if x and not isnan(x)
+                 for test in split_tests(x)]
+        print(tests)
+
+        for program in make_programs(solns, tests):
             out = ex.unsafe_check(program=program, timeout=10)
+            out["row id"] = i
             out["program"] = program
             out["functions"] = find_fns(program)
             yield out
@@ -57,6 +60,26 @@ def eval_dataset(filename: str, n_samples: int) -> Generator[Dict, None, None]:
 def find_fns(text: str) -> List[str]:
     return re.findall(r"def (.+)\(.*\)", text)
 
+
+def find_tests(text: str) -> List[str]:
+    return re.findall(r"def (test_.+)\(.*\)", text)
+
+
+def split_tests(source_code):
+    blocks = ["def test_" + x 
+              for x in source_code.split("def test_")[1:] 
+              if x.strip()]
+    out = []
+    for block in blocks:
+        lines = block.split("\n")
+        blocktext = lines[0] + "\n"
+        for line in lines[1:]:
+            if line.startswith("    "):
+                blocktext += line + "\n"
+            else:
+                break
+        out.append(blocktext)
+    return out
 
 
 if __name__ == "__main__":
