379a6b611568f804150b526ada84bc9ba758d242
David J. Lee <djslzx@gmail.com>
2023-10-25 11:51:57 -0400

    decouple logging from search

--- src/wizard.py
+++ src/wizard.py
@@ -114,52 +114,39 @@ def check_problem_solvable(chat: ChatOpenAI, problem: str) -> str:
 # generate a dataset using Evol-Instruct from the WizardLM paper
 def evol_instruct(chat: ChatOpenAI, iters: int, seed_dataset: List[str], mutators: List[str]) -> Iterator[dict]:
     dataset = seed_dataset
-    idgen = util.IdGen()
-    with get_openai_callback() as cb:
-        log_table = wandb.Table(columns=["root name", "iter", "parent name", "name", "text"])
-        for i, text in enumerate(dataset, start=1):
-            # Evolve each initial datapoint independently of the others
-            root_id = idgen.next()
-            root_name = propose_name(chat, text)
+    ids = util.IdGen()
+    for i, text in enumerate(dataset, start=1):
+        # Evolve each initial datapoint independently of the others
+        root_id = ids.next()
+        root_name = propose_name(chat, text)
+        yield {
+            "id": root_id,
+            "iter": 0,
+            "root": root_id,
+            "parent": root_id,
+            "mutator": None,
+            "name": root_name,
+            "text": text,
+        }
+        parent_id = root_id
+        parent_name = root_name
+        for gen in range(1, iters + 1):
+            mutator = random.choice(mutators)
+            text = mutate_problem(chat, text, mutator)
+            name = propose_name(chat, text)
+            id = ids.next()
             yield {
-                "id": root_id,
-                "iter": 0,
+                "id": id,
+                "iter": gen,
                 "root": root_id,
-                "parent": root_id,
-                "mutator": None,
-                "name": root_name,
+                "parent": parent_id,
+                "parent name": parent_name,
+                "mutator": mutator_name(mutator),
                 "text": text,
+                "name": name,
             }
-            parent_id = root_id
-            parent_name = root_name
-            for gen in range(1, iters + 1):
-                mutator = random.choice(mutators)
-                text = mutate_problem(chat, text, mutator)
-                name = propose_name(chat, text)
-                id = idgen.next()
-                yield {
-                    "id": id,
-                    "iter": gen,
-                    "root": root_id,
-                    "parent": parent_id,
-                    "mutator": mutator_name(mutator),
-                    "text": text,
-                    "name": name,
-                }
-                log_table.add_data(root_name, gen, parent_name, name, text)
-                wandb.log({
-                    "iter": i,
-                    "root": root_id,
-                    "names": copy.copy(log_table),
-                    "token usage": {
-                        "total cost": cb.total_cost,
-                        "total tokens": cb.total_tokens,
-                        "prompt tokens": cb.prompt_tokens,
-                        "completion tokens": cb.completion_tokens,
-                    },
-                })
-                parent_id = id
-                parent_name = name
+            parent_id = id
+            parent_name = name
 
 
 def embed(ft: feat.Featurizer, texts: Union[List[str], np.ndarray], batch_size=128, saveto=None) -> np.ndarray:
@@ -182,7 +169,7 @@ def novel_instruct(chat: ChatOpenAI,
                    seed_dataset: List[str],
                    mutators: List[str],
                    max_popn_size: int,
-                   archive_per_iter: int) -> Iterator[List[dict]]:
+                   archive_per_iter: int) -> Iterator[dict]:
     idgen = util.IdGen()
     knn = NearestNeighbors(metric="minkowski")
     e_popn = embed(ft, seed_dataset)
@@ -203,7 +190,10 @@ def novel_instruct(chat: ChatOpenAI,
             "archived?": False,
         }
         popn.append(root)
-        yield root
+        yield {
+            "kind": "data",
+            "payload": root,
+        }
     archive = []
     e_archive = []
 
@@ -223,55 +213,52 @@ def novel_instruct(chat: ChatOpenAI,
             "archived?": None,
         }
 
-    log_table = wandb.Table(columns=["top-1", "top-2", "top-3", "bot-1", "bot-2", "bot-3"])
-    with get_openai_callback() as cb:  # expose cost information
-        for i in range(1, iters + 1):
-            # take samples using evol-instruct
-            samples = np.array([take_sample(x, m)
-                                for x in popn
-                                for m in mutators], dtype=object)
+    for i in range(1, iters + 1):
+        # take samples using evol-instruct
+        samples = np.array([take_sample(x, m)
+                            for x in popn
+                            for m in mutators], dtype=object)
 
-            # use novelty pressure to select best samples
-            knn.fit(np.concatenate([e_popn, e_archive], axis=0) if archive else e_popn)
-            e_samples = embed(ft, [s["text"] for s in samples])
-            dists, _ = knn.kneighbors(e_samples)
-            dists = np.mean(dists, axis=1)
-            ranks = np.argsort(-dists)
-            i_selected = ranks[:max_popn_size]
+        # use novelty pressure to select best samples
+        knn.fit(np.concatenate([e_popn, e_archive], axis=0) if archive else e_popn)
+        e_samples = embed(ft, [s["text"] for s in samples])
+        dists, _ = knn.kneighbors(e_samples)
+        dists = np.mean(dists, axis=1)
+        ranks = np.argsort(-dists)
+        i_selected = ranks[:max_popn_size]
 
-            # update archive: take a random selection of samples and add them to the archive
-            i_archived = np.random.choice(len(samples), size=archive_per_iter, replace=False)
-            archive.extend(samples[i_archived])
-            e_archive.extend(e_samples[i_archived])
+        # update archive: take a random selection of samples and add them to the archive
+        i_archived = np.random.choice(len(samples), size=archive_per_iter, replace=False)
+        archive.extend(samples[i_archived])
+        e_archive.extend(e_samples[i_archived])
 
-            # update popn, embeddings
-            popn = samples[i_selected]
-            e_popn = e_samples[i_selected]
+        # update popn, embeddings
+        popn = samples[i_selected]
+        e_popn = e_samples[i_selected]
 
-            # log everything
-            inverted_ranks = util.invert_array(ranks)
-            log_table.add_data(*([s["name"] for s in samples[ranks[:3]]] +
-                                 [s["name"] for s in samples[ranks[-3:][::-1]]]))  # best and worst 3
-            wandb.log({
-                "scores": wandb.Histogram(dists),
-                "examples": copy.copy(log_table),
-                "token usage": {
-                    "total cost": cb.total_cost,
-                    "total tokens": cb.total_tokens,
-                    "prompt tokens": cb.prompt_tokens,
-                    "completion tokens": cb.completion_tokens,
-                },
+        # log everything
+        yield {
+            "kind": "log",
+            "payload": {
+                "scores": dists,
+                "best": [s["name"] for s in samples[ranks[:3]]],
+                "worst": [s["name"] for s in samples[ranks[-3:][::-1]]],
+            },
+        }
+
+        # update samples with score, rank, chosen?, archived?, then output samples
+        inverted_ranks = util.invert_array(ranks)
+        for j, d in enumerate(samples):
+            d.update({
+                "score": float(dists[j]),
+                "rank": int(inverted_ranks[j]),
+                "chosen?": bool(j in i_selected),
+                "archived?": bool(j in i_archived),
             })
-
-            # update samples with score, rank, chosen?, archived?, then output samples
-            for j, d in enumerate(samples):
-                d.update({
-                    "score": float(dists[j]),
-                    "rank": int(inverted_ranks[j]),
-                    "chosen?": bool(j in i_selected),
-                    "archived?": bool(j in i_archived),
-                })
-                yield d
+            yield {
+                "kind": "data",
+                "payload": d,
+            }
 
 
 def mutate_prompt(chat: ChatOpenAI, text: str, k: int) -> List[str]:
@@ -289,15 +276,25 @@ def mutate_prompt(chat: ChatOpenAI, text: str, k: int) -> List[str]:
 def run_evol_instruct(chat: ChatOpenAI, outfile: str, iters: int, seed_dataset_file: str):
     instructions = [x["instruction"] for x in json.load(open(seed_dataset_file, "r"))]
     wandb.init(project="wizard")
-    with open(outfile, "w") as f:
-        for entry in evol_instruct(
+    log_table = wandb.Table(columns=["root name", "iter", "parent name", "name", "text"])
+    with open(outfile, "w") as f, get_openai_callback() as cb:
+        for d in evol_instruct(
                 chat=chat,
                 iters=iters,
                 seed_dataset=instructions,
                 mutators=list(MUTATORS.values())):
-            s = json.dumps(entry, indent=None)
+            s = json.dumps(d, indent=None)
             print(s)
             f.write(s + "\n")
+
+            # log to wandb
+            log_table.add_data(d["root"], d["iter"], d["parent name"], d["name"], d["text"])
+            wandb.log({
+                "iter": d["iter"],
+                "root": d["root"],
+                "names": copy.copy(log_table),
+                "total cost": cb.total_cost,
+            })
 
 
 def run_novel_instruct(chat: ChatOpenAI, iters: int, seed_dataset: Union[str, List], archive_per_iter: int,
@@ -307,25 +304,43 @@ def run_novel_instruct(chat: ChatOpenAI, iters: int, seed_dataset: Union[str, Li
     else:
         assert isinstance(seed_dataset, list)
         instructions = seed_dataset
-    wandb.init(project="wizard")
+
     fe = feat.SentenceFeaturizer()
-    with open(output_file, "w") as f:
-        for entry in novel_instruct(chat, fe,
-                                    iters=iters,
-                                    seed_dataset=instructions,
-                                    mutators=list(MUTATORS.values()),
-                                    max_popn_size=max_popn_size,
-                                    archive_per_iter=archive_per_iter):
-            s = json.dumps(entry, indent=None)
-            print(s)
-            f.write(s + "\n")
+    wandb.init(project="wizard")
+    log_table = wandb.Table(columns=["top-1", "top-2", "top-3", "bot-1", "bot-2", "bot-3"])
+
+    with open(output_file, "w") as f, get_openai_callback() as cb:
+        for d in novel_instruct(chat, fe,
+                                     iters=iters,
+                                     seed_dataset=instructions,
+                                     mutators=list(MUTATORS.values()),
+                                     max_popn_size=max_popn_size,
+                                     archive_per_iter=archive_per_iter):
+            kind = d["kind"]
+            payload = d["payload"]
+
+            if kind == "data":
+                s = json.dumps(payload, indent=None)
+                print(s)
+                f.write(s + "\n")
+            elif kind == "log":
+                scores = payload["scores"]
+                best = payload["best"]
+                worst = payload["worst"]
+
+                # log to wandb
+                log_table.add_data(*(best + worst))
+                wandb.log({
+                    "scores": wandb.Histogram(scores),
+                    "examples": copy.copy(log_table),
+                    "total cost": cb.total_cost,
+                })
+            else:
+                raise ValueError(f"Unknown output kind: {kind} with payload {payload}")
 
 
 if __name__ == "__main__":
-    demo_multiple_solutions()
-    exit(0)
-
-    timestamp = datetime.datetime.now().isoformat()
+    timestamp = util.timestamp()
     iters = 2
     chat = ChatOpenAI(temperature=0.9, model_name="gpt-3.5-turbo-0613")
 
