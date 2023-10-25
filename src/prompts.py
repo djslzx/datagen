0ad480d5946c7775e97251e518aecf705a2bcf09
David J. Lee <djslzx@gmail.com>
2023-10-25 00:23:38 -0400

    python check

--- src/prompts.py
+++ src/prompts.py
@@ -93,37 +93,39 @@ def n_tests(chat: ChatOpenAI, problem: str, n: int) -> str:
     )
 
 
+def check_if_python(chat: ChatOpenAI, problem: str) -> str:
+    return run_saved_prompt(
+        chat,
+        key="check if python",
+        problem=problem,
+    )
+
+
 def gen_solns_and_tests(chat: ChatOpenAI, problems: Iterable[Tuple[int, str]]) -> Generator[
     Tuple[int, str, str], None, None]:
     for id, problem in problems:
         yield id, "original problem", problem
 
-        orig_solns = n_solns(chat, problem=problem, n=3)
-        yield id, "original solutions", orig_solns
-
-        for i, soln in enumerate(util.split_py_markdown(orig_solns)):
-            yield id, f"original solution {i}", soln
-
-        orig_tests = n_tests(chat, problem=problem, n=3)
-        yield id, "original tests", orig_tests
-
-        for i, test in enumerate(util.split_tests(orig_tests)):
-            yield id, f"original test {i}", test
+        # # skip SQL problems, web scraping problems, etc.
+        # pythonable = check_if_python(chat, problem=problem)
+        # yield id, "pythonable", pythonable
+        # if pythonable == "False":
+        #     continue
 
         restyled = restyle_problem(chat, problem=problem)
         yield id, "restyled problem", restyled
 
         restyled_solns = n_solns(chat, problem=restyled, n=3)
-        yield id, "restyled solutions", restyled_solns
+        yield id, "solutions", restyled_solns
 
         for i, soln in enumerate(util.split_py_markdown(restyled_solns)):
-            yield id, f"restyled solution {i}", soln
+            yield id, f"solution {i}", soln
 
         restyled_tests = n_tests(chat, problem=restyled, n=3)
-        yield id, "restyled tests", restyled_tests
+        yield id, "tests", restyled_tests
 
         for i, test in enumerate(util.split_tests(restyled_tests)):
-            yield id, f"restyled test {i}", test
+            yield id, f"test {i}", test
 
 
 def gen_solns_and_tests_dict(chat: ChatOpenAI, problems: List[dict]) -> Generator[dict, None, None]:
@@ -139,55 +141,35 @@ def gen_solns_and_tests_dict(chat: ChatOpenAI, problems: List[dict]) -> Generato
 if __name__ == "__main__":
     PROBLEMS = [
         """
-        Design a class called "Triangle" that represents a triangle. The "Triangle" class should have the following attributes:
-        - "side1" (an integer) - The length of side 1 of the triangle.
-        - "side2" (an integer) - The length of side 2 of the triangle.
-        - "side3" (an integer) - The length of side 3 of the triangle.
-
-        The "Triangle" class should also have the following methods:
-
+        Design a class called "Triangle" that represents a triangle.  The "Triangle" class should also have the following methods:
         - "__init__" - Initializes a new instance of the "Triangle" class with the given lengths of all sides.
         - "get_perimeter" - Returns the perimeter of the triangle, which is calculated by adding all three side lengths together.
         - "get_area" - Returns the area of the triangle, which is calculated using Heron's formula: 
         area = sqrt(s * (s - a) * (s - b) * (s - c)), where s is the semiperimeter of the triangle and a, b, and c are the lengths of the sides.
-
-        Implement the "Triangle" class in Python. Make sure to include a sample usage of the "Triangle" class to demonstrate its functionality.
-
-        For example:
-
-        t1 = Triangle(3, 4, 5)
-        print(t1.get_perimeter())  # Output: 12
-        print(t1.get_area())  # Output: 6.0
         """,
         """
         Write a function `multiplyArrays(arr1, arr2)` that takes in two arrays `arr1` and `arr2` of equal length, and returns an array `result` where each element `result[i]` is the product of `arr1[i]` and `arr2[i]`. 
         For example, given `arr1 = [2, 3, 4]` and `arr2 = [5, 6, 7]`, the function should return `[10, 18, 28]` since `10 = 2 * 5`, `18 = 3 * 6`, and `28 = 4 * 7`. 
-        Your implementation should have a time complexity of O(n), where n is the length of the input arrays.
         """,
         """
         Given a binary tree, validate if it is a binary search tree (BST).
-
-        A BST is defined as follows:
-        - The left subtree of a node contains only nodes with keys less than the node's key.
-        - The right subtree of a node contains only nodes with keys greater than the node's key.
-        - Both the left and right subtrees must also be binary search trees.
-        
-        For example, given the following tree:
-        
-                5
-               / \
-              3   7
-             / \
-            1   4
-        
-        The output should be `true` since this tree satisfies the BST property.
-        
-        Note:
-        - Assume that each node in the tree has a unique key value.
-        - The tree may be unbalanced.
         """,
+        """
+        Write a SQL query to find all the users in a database with age greater than 25.
+        """,
+        """
+        Write a function in Java that takes an integer N as input and generates the modified Fibonacci sequence up to N numbers.
+        """
     ]
     CHAT = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k-0613")
-    ts = util.timestamp()
+    for problem in PROBLEMS:
+        restyled = restyle_problem(CHAT, problem)
+        print(
+            problem,
+            restyled,
+            check_if_python(CHAT, problem=problem),
+            check_if_python(CHAT, problem=restyled)
+        )
+
     for x in gen_solns_and_tests(CHAT, enumerate(PROBLEMS)):
         print(x)
