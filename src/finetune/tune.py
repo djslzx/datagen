8d052c77b0c9df40bf17ddb014cffddbd48608fb
David Jinsoo Lee <djslzx@gmail.com>
2023-11-20 14:36:00 -0500

    fix run script, fix tokenizer padding token

--- src/finetune/tune.py
+++ src/finetune/tune.py
@@ -9,11 +9,16 @@ import torch as T
 from tqdm import tqdm
 import wandb
 import sys
+from datetime import datetime
 
 # hugging face
 from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModel, TrainingArguments
 from trl import SFTTrainer, DataCollatorForCompletionOnlyLM
 import datasets
+
+
+def timestamp():
+    return datetime.now().isoformat()
 
 
 def load_llama() -> Tuple[AutoModel, AutoTokenizer]:
@@ -56,6 +61,7 @@ def massage_solved_dataset(infile: str, outfile_prefix: str):
     df["id"] = df["id"].apply(rename)
     df = df[df["key"].isin(["id", "original problem", "restyled problem"] + soln_cols(3))]
     df = df.drop_duplicates(subset=["id", "key"], keep="first")
+
     df = df.pivot(index="id", columns="key", values="value")
     df = df.where(pd.notnull(df), None)
     df["source"] = df.index.map(lambda x: x.split(":")[0])
@@ -82,24 +88,13 @@ def massage_solved_dataset(infile: str, outfile_prefix: str):
         out.to_json(f"{outfile_prefix}-{source}.jsonl", orient="records", lines=True)
 
 
-def load_dataset(filename: str) -> datasets.Dataset:
+def load_json_dataset(filename: str) -> datasets.Dataset:
     assert filename.endswith(".jsonl"), \
         f"Expected jsonl file, but got filename={filename}"
     
     df = pd.read_json(filename, lines=True)
     data = datasets.Dataset.from_pandas(df)
     return data
-
-
-def demo_llama():
-    model, tokenizer = load_llama()
-    input_text = [
-        "#PROBLEM\Write a function in Python to generate the first n Fibonacci numbers.\n",
-        "#SOLUTION\n",
-    ]
-    input_tokens = tokenizer.encode(input_text, return_tensors="pt")
-    output = model.generate(input_tokens, max_length=1000, do_sample=True, top_p=0.95, top_k=60)
-    print(tokenizer.decode(output[0], skip_special_tokens=True))
 
 
 def tune_all(model: AutoModel, tokenizer: AutoTokenizer, train_datasets: List[str]):
@@ -109,35 +104,65 @@ def tune_all(model: AutoModel, tokenizer: AutoTokenizer, train_datasets: List[st
     pass
 
 
-def tune_once(model: AutoModel, tokenizer: AutoTokenizer, dataset: datasets.Dataset, dataset_name: str,
-              max_seq_length=2048):
+def tune_once(model: AutoModel, 
+              tokenizer: AutoTokenizer, 
+              dataset_name: str,
+              dataset: datasets.Dataset, 
+              max_seq_length=2048,
+              problem_key = "restyled problem",
+):
+    def format_prompt(q: str, a: str) -> str:
+        return f"# Question: {q}\n# Answer: {a}"
 
     def format_prompts(x) -> List[str]:
+        """
+        Format each question-answer pair as a single string with headers.
+        These will be split later using the response template
+        """
         outputs = []
-        problems, solutions = x["restyled problem"], x["solution"]
+        problems, solutions = x[problem_key], x["solution"]
         assert len(problems) == len(solutions), \
             (f"Expected to have the same number of problems and solutions, but got "
              f"|problems|={len(problems)}, |solutions|={len(solutions)}")
         for problem, solution in zip(problems, solutions):
-            outputs.append(f"# Question: {problem}\n "
-                           f"# Answer: {solution}")
+            outputs.append(format_prompt(problem, solution))
         return outputs
 
-    if not tokenizer.pad_token:
-        print("WARNING: no pad token defined by tokenizer, setting to default value", file=sys.stderr)
-        # fixme: not sure if this is the right pad token to use, as the original tokenizer has no padding token
-        tokenizer.pad_token = "<|pad|>"
+    # todo
+    def compute_metrics(eval_predictions):
+        pass
 
-    # filter out elements in the dataset that are longer than the cutoff
-    dataset = dataset.filter(lambda x: len(tokenizer.encode(x["restyled problem"] + x["solution"])) < max_seq_length)
+    assert problem_key in {"original problem", "restyled problem"}, \
+        (f"Invalid problem key {problem_key}: must be one of "
+         "{'original problem', 'restyled problem'}")
+
+    ts = timestamp()
+
+    if not tokenizer.pad_token:
+        print("WARNING: no pad token defined by tokenizer, setting to [PAD]", file=sys.stderr)
+        tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+        model.resize_token_embeddings(len(tokenizer))
+
+    print(tokenizer.eos_token, tokenizer.eos_token_id,
+          tokenizer.pad_token, tokenizer.pad_token_id)
+
+    tokenizer.truncation = True
+    tokenizer.padding = "max_length"
+
+    # # filter out elements in the dataset that are longer than the cutoff
+    # dataset = dataset.filter(lambda x:
+    #                          len(tokenizer.encode(format_prompt(x[problem_key], 
+    #                                                             x["solution"]))) < max_seq_length)
 
     # Compensate for lack of context in response template 
     response_template_w_context = "\n# Answer:"
-    response_template_ids = tokenizer.encode(response_template_w_context, add_special_tokens=False)[2:]
+    response_template_ids = tokenizer.encode(response_template_w_context, 
+                                             add_special_tokens=False)[2:]
     collator = DataCollatorForCompletionOnlyLM(response_template_ids, tokenizer=tokenizer)
     args = TrainingArguments(
-        output_dir=f"{dataset_name}-output",
+        output_dir=f"../models/ft/{dataset_name}/{ts}",
         per_device_train_batch_size=2,
+        bf16=True,
     )
 
     wandb.init(project="sft")
@@ -150,7 +175,6 @@ def tune_once(model: AutoModel, tokenizer: AutoTokenizer, dataset: datasets.Data
         max_seq_length=max_seq_length,
     )
     trainer.train()
-    trainer.save_model(f"output/model-ft-{dataset_name}")
     
 
 if __name__ == "__main__":
@@ -167,7 +191,6 @@ if __name__ == "__main__":
     # massage_solved_dataset(f"{root}/all-solved-20k:30k.jsonl",
     #                        f"{root}/paired-20k:30k")
 
-    dataset = load_dataset(filename)
-    # model, tokenizer = load_llama()
+    dataset = load_json_dataset(filename)
     model, tokenizer = load_dummy_model()
-    tune_once(model, tokenizer, dataset, dataset_name=name)
+    tune_once(model, tokenizer, dataset_name=name, dataset=dataset, max_seq_length=1024)
