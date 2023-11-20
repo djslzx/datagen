b87f216595476f0aa82a945c4e118b25e48a675b
David Jinsoo Lee <djslzx@gmail.com>
2023-11-20 00:52:48 -0500

    tune once

--- src/finetune/tune.py
+++ src/finetune/tune.py
@@ -102,54 +102,41 @@ def demo_llama():
     print(tokenizer.decode(output[0], skip_special_tokens=True))
 
 
-def tuning(model: AutoModel, tokenizer: AutoTokenizer, dataset: datasets.Dataset):
+def tune_all(model: AutoModel, tokenizer: AutoTokenizer, train_datasets: List[str]):
     """
-    Train on X = {WW, WD, CA, NSCA, NSE}
-    and test on Y = union X {HumanEval, DS1000, APPS, MBPP}
+    Finetune multiple versions of the model, one for each training dataset.
     """
     pass
-    
-
-def format_prompts(x) -> List[str]:
-    outputs = []
-    problems, solutions = x["restyled problem"], x["solution"]
-    assert len(problems) == len(solutions), \
-        (f"Expected to have the same number of problems and solutions, but got "
-         f"|problems|={len(problems)}, |solutions|={len(solutions)}")
-    for problem, solution in zip(problems, solutions):
-        outputs.append(f"# Question: {problem}\n "
-                       f"# Answer: {solution}")
-    return outputs
 
 
-if __name__ == "__main__":
-    pd.set_option("display.max_columns", None)
-    pd.set_option("display.max_colwidth", 20)
+def tune_once(model: AutoModel, tokenizer: AutoTokenizer, dataset: datasets.Dataset, dataset_name: str,
+              max_seq_length=2048):
 
-    root = "/home/djl328/prob-repl/datasets/wiz"
-    # root = "../../datasets/wiz"
+    def format_prompts(x) -> List[str]:
+        outputs = []
+        problems, solutions = x["restyled problem"], x["solution"]
+        assert len(problems) == len(solutions), \
+            (f"Expected to have the same number of problems and solutions, but got "
+             f"|problems|={len(problems)}, |solutions|={len(solutions)}")
+        for problem, solution in zip(problems, solutions):
+            outputs.append(f"# Question: {problem}\n "
+                           f"# Answer: {solution}")
+        return outputs
 
-    # massage dataset
-    massage_solved_dataset(f"{root}/all-solved-20k:30k.jsonl",
-                           f"{root}/paired-20k:30k")
-
-    # demo_llama()
-    dataset = load_dataset(f"{root}/paired-plus-20k:30k.jsonl")
-
-    # model, tokenizer = load_llama()
-    model, tokenizer = load_dummy_model()
-
-    # fixme: not sure if this is the right pad token to use, as the original tokenizer has no padding token
     if not tokenizer.pad_token:
         print("WARNING: no pad token defined by tokenizer, setting to default value", file=sys.stderr)
+        # fixme: not sure if this is the right pad token to use, as the original tokenizer has no padding token
         tokenizer.pad_token = "<|pad|>"
+
+    # filter out elements in the dataset that are longer than the cutoff
+    dataset = dataset.filter(lambda x: len(tokenizer.encode(x["restyled problem"] + x["solution"])) < max_seq_length)
 
     # Compensate for lack of context in response template 
     response_template_w_context = "\n# Answer:"
     response_template_ids = tokenizer.encode(response_template_w_context, add_special_tokens=False)[2:]
     collator = DataCollatorForCompletionOnlyLM(response_template_ids, tokenizer=tokenizer)
     args = TrainingArguments(
-        output_dir="output",
+        output_dir=f"{dataset_name}-output",
         per_device_train_batch_size=2,
     )
 
@@ -160,7 +147,27 @@ if __name__ == "__main__":
         data_collator=collator,
         formatting_func=format_prompts,
         args=args,
-        max_seq_length=2048,
+        max_seq_length=max_seq_length,
     )
     trainer.train()
-    trainer.save_model("output/ft-model")
+    trainer.save_model(f"output/model-ft-{dataset_name}")
+    
+
+if __name__ == "__main__":
+    pd.set_option("display.max_columns", None)
+    pd.set_option("display.max_colwidth", 20)
+
+    args = sys.argv[1:]
+    assert len(args) == 2, f"Expected name, filename but got {args}"
+    name, filename = args
+
+    # # massage dataset
+    # root = "/home/djl328/prob-repl/datasets/wiz"
+    # root = "../../datasets/wiz"
+    # massage_solved_dataset(f"{root}/all-solved-20k:30k.jsonl",
+    #                        f"{root}/paired-20k:30k")
+
+    dataset = load_dataset(filename)
+    # model, tokenizer = load_llama()
+    model, tokenizer = load_dummy_model()
+    tune_once(model, tokenizer, dataset, dataset_name=name)
