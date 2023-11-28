8e076c4a04f15845872ba794f8501bea52eda954
David Jinsoo Lee <djslzx@gmail.com>
2023-11-27 19:22:50 -0500

    use hf api for data

--- src/finetune/tune.py
+++ src/finetune/tune.py
@@ -1,7 +1,7 @@
 """
 Finetune an LLM using the generated datasets
 """
-from typing import Tuple, List, Dict
+from typing import Tuple, List, Dict, Callable
 from math import isnan
 import pandas as pd
 import numpy as np
@@ -10,11 +10,13 @@ from tqdm import tqdm
 import wandb
 import sys
 from datetime import datetime
+import argparse
+from pathlib import Path
 
 # hugging face
 from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModel, TrainingArguments
 from trl import SFTTrainer, DataCollatorForCompletionOnlyLM
-import datasets
+from datasets import Dataset, DatasetInfo, DatasetDict, load_dataset
 
 
 def timestamp():
@@ -71,7 +73,6 @@ def massage_solved_dataset(
         in_file: str, 
         out_dir: str,
         name_map: Dict[str, str] = None,
-        split: Dict[str, float] = None,
 ):
     """
     Clean up datasets consisting of problems, solutions, and checkers.
@@ -79,7 +80,7 @@ def massage_solved_dataset(
     - rename datasets, e.g. CA-20k to CA
     - extract source from id
     - split into separate output files by source
-    - train/validation/test split
+    - shuffle dataset
     """
     assert in_file.endswith(".jsonl"), f"Expected jsonl file, but got in_file={in_file}"
 
@@ -92,14 +93,7 @@ def massage_solved_dataset(
             "Wiz-wide": "WW",
         }
 
-    if not split:
-        split = {
-            "train": 0.8,
-            "validation": 0.1,
-            "test": 0.1,
-        }
-
-    def soln_cols(n: int) -> List[str]:
+    def soln_keys(n: int) -> List[str]:
         return [f"solution {i}" for i in range(n)]
 
     def rename(s_id: str) -> str:
@@ -107,17 +101,17 @@ def massage_solved_dataset(
         s_src = name_map[s_src] if s_src in name_map else s_src
         return f"{s_src}:{s_num}"
 
+    n_solns = 3
     df = pd.read_json(in_file, lines=True)
     df = df[["id", "key", "value"]]
     df["id"] = df["id"].apply(rename)
-    df = df[df["key"].isin(["id", "original problem", "restyled problem"] + soln_cols(3))]
+    df = df[df["key"].isin(["id", "original problem", "restyled problem"] + soln_keys(n_solns))]
     df = df.drop_duplicates(subset=["id", "key"], keep="first")
 
     df = df.pivot(index="id", columns="key", values="value")
     df = df.where(pd.notnull(df), None)
     df["source"] = df.index.map(lambda x: x.split(":")[0])
 
-    # split solns into separate rows: each id should be `file:problem-id:soln-id`
     # fixme: for now, we make the simplifying assumption that all solutions
     #   are good, so use any problem/solution pair to fine-tune
 
@@ -125,11 +119,12 @@ def massage_solved_dataset(
     df = df.sample(frac=1)
 
     # split each source file into its own dataset
-    for source in df["source"].unique():
+    for source in sorted(df["source"].unique()):
         data = df[df["source"] == source]
         rows = []
-        for id, row in tqdm(data.iterrows(), total=len(data), desc=f"Massaging dataset={source}"):
-            for i, soln in enumerate(soln_cols(3)):
+        print(f"Found {len(data)} lines in {source}, processing...", file=sys.stderr)
+        for id, row in tqdm(data.iterrows(), total=len(data), desc=f"Massaging {source}"):
+            for i, soln in enumerate(soln_keys(n_solns)):
                 if row[soln]:
                     rows.append({
                         "id": f"{id}:{i}",
@@ -138,19 +133,21 @@ def massage_solved_dataset(
                         "restyled problem": row["restyled problem"],
                         "solution": row[soln],
                     })
-        # train/validate/test split
-        for name, subset in split_by_percentages(rows, split).items():
-            out = pd.DataFrame.from_records(subset)
-            out.to_json(f"{out_dir}/{source}/{name}.jsonl", orient="records", lines=True)
-
-
-def load_json_dataset(filename: str) -> datasets.Dataset:
-    assert filename.endswith(".jsonl"), \
-        f"Expected jsonl file, but got filename={filename}"
-    
-    df = pd.read_json(filename, lines=True)
-    data = datasets.Dataset.from_pandas(df)
-    return data
+        ds = Dataset.from_pandas(
+            pd.DataFrame.from_records(rows),
+            info=DatasetInfo(
+                dataset_name=source,
+                description=f"{source} dataset",
+            )
+        )
+        tt = ds.train_test_split(test_size=0.2)
+        vt = tt["test"].train_test_split(test_size=0.5)
+        dd = DatasetDict({
+            "train": tt["train"],
+            "validation": vt["train"],
+            "test": vt["test"],
+        })
+        dd.save_to_disk(f"{out_dir}/{source}")
 
 
 def tune_all(model: AutoModel, tokenizer: AutoTokenizer, train_datasets: List[str]):
@@ -162,8 +159,7 @@ def tune_all(model: AutoModel, tokenizer: AutoTokenizer, train_datasets: List[st
 
 def tune_once(model: AutoModel, 
               tokenizer: AutoTokenizer, 
-              dataset_name: str,
-              dataset: datasets.Dataset, 
+              dataset: Dataset, 
               max_seq_length=2048,
               problem_key = "restyled problem",
 ):
@@ -171,10 +167,6 @@ def tune_once(model: AutoModel,
         return f"# Question: {q}\n# Answer: {a}\n#DONE#"
 
     def format_prompts(x) -> List[str]:
-        """
-        Format each question-answer pair as a single string with headers.
-        These will be split later using the response template
-        """
         outputs = []
         problems, solutions = x[problem_key], x["solution"]
         assert len(problems) == len(solutions), \
@@ -195,28 +187,19 @@ def tune_once(model: AutoModel,
     ts = timestamp()
 
     if not tokenizer.pad_token:
-        print("WARNING: no pad token defined by tokenizer, setting to [PAD]", file=sys.stderr)
+        print("WARNING: no pad token defined by tokenizer, setting to default", file=sys.stderr)
         tokenizer.add_special_tokens({'pad_token': '[PAD]'})
         model.resize_token_embeddings(len(tokenizer))
 
-    print(tokenizer.eos_token, tokenizer.eos_token_id,
-          tokenizer.pad_token, tokenizer.pad_token_id)
-
-    tokenizer.truncation = True
-    tokenizer.padding = "max_length"
-
-    # # filter out elements in the dataset that are longer than the cutoff
-    # dataset = dataset.filter(lambda x:
-    #                          len(tokenizer.encode(format_prompt(x[problem_key], 
-    #                                                             x["solution"]))) < max_seq_length)
-
     # Compensate for lack of context in response template 
     response_template_w_context = "\n# Answer:"
-    response_template_ids = tokenizer.encode(response_template_w_context, 
-                                             add_special_tokens=False)[2:]
+    response_template_ids = tokenizer(response_template_w_context,
+                                      truncation=True,
+                                      padding="max_length",
+                                      add_special_tokens=False)[2:]
     collator = DataCollatorForCompletionOnlyLM(response_template_ids, tokenizer=tokenizer)
     args = TrainingArguments(
-        output_dir=f"../models/ft/{dataset_name}/{ts}",
+        output_dir=f"../models/ft/{dataset.info.dataset_name}/{ts}",
         per_device_train_batch_size=2,
         bf16=True,
     )
@@ -234,21 +217,29 @@ def tune_once(model: AutoModel,
     
 
 if __name__ == "__main__":
-    # test_split_by_percentages()
-    pd.set_option("display.max_columns", None)
-    pd.set_option("display.max_colwidth", 20)
+    p = argparse.ArgumentParser()
+    p.add_argument("mode", type=str, choices=["data", "tune"])
+    p.add_argument("--file", type=str)
+    p.add_argument("--out-dir", type=str)
+    p.add_argument("--model", type=str)
 
-    args = sys.argv[1:]
-    assert len(args) == 2, f"Expected name, filename but got {args}"
-    name, filename = args
+    args = p.parse_args()
 
-    # massage dataset
-    root = "/home/djl328/prob-repl/datasets/wiz"
-    # root = "../../datasets/wiz"
-    massage_solved_dataset(in_file=f"{root}/all-solved-20k:30k.jsonl",
-                           out_dir=f"{root}/paired-20k:30k")
-    exit(0)
+    if args.mode == "data":
+        # Path(args.out_dir).mkdir(parents=True, exist_ok=False)
+        massage_solved_dataset(in_file=args.file, out_dir=args.out_dir)
+    elif args.mode == "tune":
+        dataset = load_dataset("json", data_files=args.file)
+        model, tokenizer = load_dummy_model()
+        tune_once(model, tokenizer, dataset=dataset, max_seq_length=1024)
 
-    dataset = load_json_dataset(filename)
-    model, tokenizer = load_dummy_model()
-    tune_once(model, tokenizer, dataset_name=name, dataset=dataset, max_seq_length=1024)
+    # todos:
+    # - add validation loss to metrics during ft
+    # - sweep using wandb
+    # - set up memorization test (100 examples)
+    # - while sweep runs:
+    #   - set up test/eval framework
+    #     - use standard datasets (HumanEval, DS1000, APPS, MBPP)
+    #     - report %checkers passed for each of n solutions sampled per problem
+    #   - set up problem distribution visualization
+
