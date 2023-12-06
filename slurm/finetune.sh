7dba72df1f43c201ddfeb4b8f7f668a635effee9
David Jinsoo Lee <djslzx@gmail.com>
2023-12-06 10:48:15 -0500

    update run script for module changes

--- slurm/finetune.sh
+++ slurm/finetune.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-prefix="/home/djl328/prob-repl"
+PROJECT_DIR="/home/djl328/prob-repl"
 export 'PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:512'
 export 'TOKENIZERS_PARALLELISM=false'
-source $prefix/venv/bin/activate
-cd $prefix/src
-python3 -u finetune/tune.py $@
+export PYTHONPATH=$PYTHONPATH:$PROJECT_DIR/src
+source $PROJECT_DIR/venv/bin/activate
+cd $PROJECT_DIR/src
+python3 -u finetune/main.py $@
 
