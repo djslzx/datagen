8dabd1a45f4186d42a09bbc3bb093160bcc9d298
David J. Lee <djslzx@gmail.com>
2023-12-06 11:15:04 -0500

    add tests to each problem/solution pair

--- src/finetune/data.py
+++ src/finetune/data.py
@@ -13,12 +13,15 @@ def clean_solution_dataset(
         in_file: str,
         out_dir: str,
         name_map: Dict[str, str] = None,
+        n_solns: int = 3,
+        n_tests: int = 3,
 ):
     """
     Clean up datasets consisting of problems, solutions, and checkers.
     - pivot from kv to columnar form
     - rename datasets, e.g. CA-20k to CA
     - extract source from id
+    - add columns for problems, solutions, and tests
     - split into separate output files by source
     - shuffle dataset
     """
@@ -33,19 +36,20 @@ def clean_solution_dataset(
             "Wiz-wide": "WW",
         }
 
-    def soln_keys(n: int) -> List[str]:
-        return [f"solution {i}" for i in range(n)]
+    problem_keys = ["restyled problem"]
+    soln_keys = [f"solution {i}" for i in range(n_solns)]
+    test_keys = [f"test {i}" for i in range(n_tests)]
 
     def rename(s_id: str) -> str:
         s_src, s_num = s_id.split(":")
         s_src = name_map[s_src] if s_src in name_map else s_src
         return f"{s_src}:{s_num}"
 
-    n_solns = 3
     df = pd.read_json(in_file, lines=True)
     df = df[["id", "key", "value"]]
     df["id"] = df["id"].apply(rename)
-    df = df[df["key"].isin(["id", "restyled problem"] + soln_keys(n_solns))]
+    keys = ["id"] + problem_keys + soln_keys + test_keys
+    df = df[df["key"].isin(keys)]
     df = df.drop_duplicates(subset=["id", "key"], keep="first")
 
     df = df.pivot(index="id", columns="key", values="value")
@@ -64,13 +68,15 @@ def clean_solution_dataset(
         rows = []
         print(f"Found {len(data)} lines in {source}, processing...", file=sys.stderr)
         for id, row in tqdm(data.iterrows(), total=len(data), desc=f"Massaging {source}"):
-            for i, soln in enumerate(soln_keys(n_solns)):
+            for i, soln in enumerate(soln_keys):
+                tests = [row[t] for t in test_keys]
                 if row[soln]:
                     rows.append({
                         "id": f"{id}:{i}",
                         "source": source,
                         "problem": row["restyled problem"],
                         "solution": row[soln],
+                        "tests": tests,
                     })
         ds = Dataset.from_pandas(
             pd.DataFrame.from_records(rows),
