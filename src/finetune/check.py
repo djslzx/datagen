d8c16607e6932cd19f7f6154fd1110d28ab3260c
David J. Lee <djslzx@gmail.com>
2023-12-13 10:19:35 -0500

    filtering stub in check.py

--- src/finetune/check.py
+++ src/finetune/check.py
@@ -1,5 +1,3 @@
-# dirty import hacks...
-import sys, os
 from typing import Dict, List
 import pandas as pd
 import numpy as np
@@ -7,3 +5,21 @@ from datasets import Dataset
 
 from root import evaluate
 import finetune.data as data
+import finetune.models as models
+
+
+def filter_solns_and_tests(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Filter out solutions that don't pass tests
+    Filter out tests that don't permit solutions
+    Filter out rows with no remaining solutions or tests
+    """
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    df = data.fetch_solns_and_tests("../../datasets/wiz/all-solved/all-solved-20k:30k.jsonl", source="NSCA")
+    print(len(df))
+    df = filter_solns_and_tests(df)
+    print(len(df))
+    print(df)
