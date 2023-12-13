7289fb3d940bd9f8a2bda0a8aa5f7d4bc39a7bff
David Jinsoo Lee <djslzx@gmail.com>
2023-12-13 14:21:30 -0500

    remove runners

--- slurm/runner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-PROJECT_DIR=/home/djl328/prob-repl
-export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:512
-export TOKENIZERS_PARALLELISM=false
-export PYTHONPATH=$PYTHONPATH:$PROJECT_DIR/src
-source $PROJECT_DIR/venv/bin/activate
-cd $PROJECT_DIR/src
-python3 -u evaluate.py
-
