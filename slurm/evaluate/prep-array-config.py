f0f86811ba32d74b3db61b3b238fd24c6d34da7e
David Jinsoo Lee <djslzx@gmail.com>
2024-01-05 01:10:56 -0500

    job array fix; interleave job array config

--- slurm/evaluate/prep-array-config.py
+++ slurm/evaluate/prep-array-config.py
@@ -9,19 +9,19 @@ def tabprint(*xs: List[str], file: str):
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
-    p.add_argument("n", type=int)
-    p.add_argument("o", type=str)
+    p.add_argument("n_chunks", type=int)
+    p.add_argument("outfile", type=str)
     args = p.parse_args()
 
     datasets = [
-        "NSCA",
+        # "NSCA",
         "NSE",
         "WW",
         "WD",
         "CA",
     ]
 
-    with open(args.o, "w") as f:
+    with open(args.outfile, "w") as f:
         tabprint("ID", "Dataset", file=f)
-        for i, (dataset, i_chunk) in enumerate(it.product(datasets, range(args.n)), 1):
+        for i, (i_chunk, dataset) in enumerate(it.product(range(args.n_chunks), datasets), 1):
             tabprint(i, f"{dataset}/chunk-{i_chunk:04d}.jsonl", file=f)
