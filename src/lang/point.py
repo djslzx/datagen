8731057f853021e8e77b84199ddcbd5cb177a400
David J. Lee <djslzx@gmail.com>
2024-04-03 10:07:04 -0400

    maze domain

--- src/lang/point.py
+++ src/lang/point.py
@@ -2,7 +2,7 @@
 Languages for testing novelty search with "point programs".
 Programs are 2D points, and novelty is the Euclidean distance between them.
 """
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Tuple
 import numpy as np
 import scipy.stats as stats
 
@@ -74,12 +74,16 @@ class RealPoint(Language):
             self.x_distribution = stats.gaussian_kde(xs, bw_method=self.std)
             self.y_distribution = stats.gaussian_kde(ys, bw_method=self.std)
 
-    def sample(self) -> Tree:
+    def _sample_coords(self) -> Tuple[int, int]:
         x = self.x_distribution.resample(1).item()
         y = self.y_distribution.resample(1).item()
         if self.lim is not None:
             x = min(self.lim, max(-self.lim, x))
             y = min(self.lim, max(-self.lim, y))
+        return x, y
+
+    def sample(self) -> Tree:
+        x, y = self._sample_coords()
         return self.parse(f"({x}, {y})")
 
     def log_probability(self, t: Tree) -> float:
@@ -93,9 +97,6 @@ class RealPoint(Language):
             "pos": lambda n: f"{n}",
             "neg": lambda n: f"-{n}",
         }
-
-    def log(self, popn: List[Tree]):
-        pass
 
 
 class NatPoint(Language):
@@ -145,6 +146,116 @@ class NatPoint(Language):
 
     def none(self) -> Any:
         return np.array([0, 0])
+
+
+class RealMaze(RealPoint):
+    def __init__(self, mask: np.ndarray, cell_size=1):
+        """
+        mask: defines maze shape, where 1s are walls
+        cell_size: size of each cell in the mask, 1 by default
+        """
+        assert mask.ndim == 2
+        n_rows, n_cols = mask.shape
+        assert n_rows == n_cols
+
+        super().__init__(lim=mask.shape[0] * cell_size)
+
+        self.mask = mask
+        self.n = n_rows
+
+    def _is_valid_point(self, x: float, y: float) -> bool:
+        """
+        Check if the point (x, y) is in a valid position in the maze
+        """
+        if x < 0 or y < 0:
+            return False
+
+        x, y = int(x), int(y)
+        in_bounds = 0 <= x < self.n and 0 <= y < self.n
+        in_wall = bool(self.mask[y, x])
+
+        return in_bounds and not in_wall
+
+    def sample(self) -> Tree:
+        # keep resampling until we get something that's within the bounds of the maze
+        x, y = super()._sample_coords()
+        while not self._is_valid_point(x, y):
+            x, y = super()._sample_coords()
+        return self.parse(f"({x}, {y})")
+
+
+def str_to_float_mask(str_mask: List[str]) -> np.ndarray:
+    """
+    Convert a list of strings into a binary float array,
+    where any '#' chars are interpreted as 1, any other char
+    is interpreted as 0.
+
+    Change from row-major form (row, col) to (x, y), where
+      x = c
+      y = n - r
+    i.e., a single 90deg clockwise rotation.
+    """
+    m = np.array([
+        [
+            float(c == "#")
+            for c in line
+        ] for line in str_mask
+    ])
+    # rotate once clockwise
+    return np.rot90(m, axes=(1, 0))
+
+
+def test_str_to_float_mask():
+    s = ["#_",
+         "__"]
+    m = str_to_float_mask(s)
+    assert m[0, 0] == 0.
+    assert m[0, 1] == 1.
+    assert m[1, 0] == 0.
+    assert m[1, 1] == 0.
+
+    s = ["_#",
+         "__"]
+    m = str_to_float_mask(s)
+    assert m[0, 0] == 0.
+    assert m[0, 1] == 0.
+    assert m[1, 0] == 0.
+    assert m[1, 1] == 1.
+
+    s = ["__",
+         "#_"]
+    m = str_to_float_mask(s)
+    assert m[0, 0] == 1.
+    assert m[0, 1] == 0.
+    assert m[1, 0] == 0.
+    assert m[1, 1] == 0.
+
+    s = ["__",
+         "_#"]
+    m = str_to_float_mask(s)
+    assert m[0, 0] == 0.
+    assert m[0, 1] == 0.
+    assert m[1, 0] == 1.
+    assert m[1, 1] == 0.
+
+
+def test_maze():
+    str_mask = [
+        "________",
+        "_######_",
+        "_#______",
+        "_#____#_",
+        "_#_####_",
+        "_#____#_",
+        "_###__#_",
+        "___#__#_",
+    ]
+    mask = str_to_float_mask(str_mask)
+    maze = RealMaze(mask)
+    print(maze.mask)
+    for i in range(10):
+        tree = maze.sample()
+        print(tree)
 
 
 class PointFeaturizer(Featurizer):
