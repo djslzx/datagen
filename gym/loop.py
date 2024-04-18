88316090ae780e8072308a6e6912fa76f84797f3
David Jinsoo Lee <djslzx@gmail.com>
2024-04-18 07:17:45 -0400

    update gym

--- gym/loop.py
+++ gym/loop.py
@@ -1,7 +1,8 @@
 import gymnasium as gym
+from gymnasium.utils.save_video import save_video
 # import gymnasium_robotics
 
-example_map = [
+cross_map = [
     [1, 1, 1, 1, 1],
     [1, 1, 0, 1, 1],
     [1, 0, 0, 0, 1],
@@ -9,14 +10,25 @@ example_map = [
     [1, 1, 1, 1, 1],
 ]
 
-env = gym.make('AntMaze_UMaze-v4')
-observation, info = env.reset()
+env = gym.make('AntMaze_UMaze-v4', maze_map=cross_map, render_mode="rgb_array")
 
-for _ in range(1000):
+_ = env.reset()
+step_start_i = 0
+episode_i = 0
+for step_i in range(1000):
     action = env.action_space.sample()  # agent policy that uses the observation and info
-    observation, reward, terminated, truncated, info = env.step(action)
+    _, _, terminated, truncated, _ = env.step(action)
 
     if terminated or truncated:
+        save_video(
+            env.render(),
+            "videos",
+            fps=env.metadata["render_fps"],
+            step_starting_index=step_starting_index,
+            episode_index = episode_index,
+        )
+        step_start_i = step_i + 1
+        episode_i += 1
         observation, info = env.reset()
 
 env.close()
