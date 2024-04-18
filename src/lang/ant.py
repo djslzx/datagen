7751cae3af4cb3d0c120a2a9155eae13b6829eea
David Jinsoo Lee <djslzx@gmail.com>
2024-04-18 15:07:04 -0400

    merge together ant grammar and mujoco execution; clean up old ant

--- src/lang/ant.py
+++ src/lang/ant.py
@@ -5,75 +5,20 @@ import numpy as np
 import einops as ein
 from scipy.special import softmax
 import torch
+import torch.nn as nn
 import gymnasium as gym
 from gymnasium.utils.save_video import save_video
+from tqdm import tqdm
 
 from lang.tree import Language, Tree, Grammar, ParseError, Featurizer
-from lang.sac_core import SquashedGaussianMLPActor
-
-
-# class Ant(Language):
-#     """
-#     Mujoco ant domain from Programmatic RL without Oracles.
-#     """
-#     grammar = r"""
-#         e: "if" "(" b ")" "then" vec "else" "("? e ")"? -> if
-#          | vec                                          -> c
-#         b: NUMBER "+" vec "* X >= 0"                    -> b
-#         vec: "[" (NUMBER ","?)* "]"                     -> vec
-        
-#         %import common.NUMBER
-#         %import common.WS
-#         %ignore WS
-#     """
-
-#     def __init__(self, env_dim: int, primitive_policies: np.ndarray, featurizer: Featurizer):
-#         raise NotImplementedError
-
-#         super().__init__(
-#             parser_grammar=Ant.grammar,
-#             parser_start="e",
-#             root_type=None,
-#             model=None,
-#             featurizer=featurizer,
-#         )
-#         self.env_dim = env_dim
-#         assert primitive_policies.ndim == 2
-#         # print(f"Loaded primitive policies with shape {primitive_policies.shape}")
-#         self.primitive_policies = primitive_policies
-
-#     def _extract_weight_map(self, t: Tree) -> np.ndarray:
-#         raise NotImplementedError
-
-#     def sample(self) -> Tree:
-#         raise NotImplementedError
-
-#     def fit(self, corpus: List[Tree], alpha):
-#         raise NotImplementedError
-
-#     def eval(self, t: Tree, env: Dict[str, Any] = None) -> Any:
-#         raise NotImplementedError
-
-#         assert "state" in env, "Ant must evaluate on an env state"
-#         env_state = env["state"]
-#         assert isinstance(env_state, np.ndarray)
-#         assert env_state.ndim == 1
-#         assert env_state.shape[0] == self.env_dim
-
-#     @property
-#     def str_semantics(self) -> Dict:
-#         return {
-#             "if": lambda b, c, e: f"if ({b}) then {c} else ({e})",
-#             "c": lambda v: f"{v}",
-#             "b": lambda n, v: f"{n} + {v} * X >= 0",
-#             "vec": lambda *xs: "[ " + " ".join(xs) + " ]",
-#         }
+from spinup.algos.pytorch.sac.core import SquashedGaussianMLPActor
 
 
 class FixedDepthAnt(Language):
     """
     Mujoco ant domain from Programmatic RL without Oracles, with fixed tree depth.
     """
+    # grammar with list structure
     grammar = r"""
         root: "(" "ant" "(" "conds" conds ")" \
                         "(" "stmts" stmts ")" ")" -> root
@@ -86,15 +31,34 @@ class FixedDepthAnt(Language):
         %ignore WS
     """
 
+    # grammar with if/else tree structure
+    # grammar = r"""
+    #     e: "if" "(" b ")" "then" vec "else" "("? e ")"? -> if
+    #      | vec                                          -> c
+    #     b: NUMBER "+" vec "* X >= 0"                    -> b
+    #     vec: "[" (NUMBER ","?)* "]"                     -> vec
+    #     %import common.NUMBER
+    #     %import common.WS
+    #     %ignore WS
+    # """
+    
     def __init__(
             self, 
             env_dim: int, 
-            n_primitives: int, 
             depth: int, 
+            primitives: List[nn.Module],
+            maze_map: List[List[int]],
+            steps: int,
             featurizer: Featurizer,
             structure_params=False
     ):
         assert depth > 1
+        assert steps > 0
+        assert env_dim > 0
+        assert all(cell == 0 or cell == 1
+                   for row in maze_map for cell in row), \
+                           f"Expected binary map but got {maze_map}"
+
         self.n_conds = depth - 1
         self.n_stmts = depth
 
@@ -107,11 +71,22 @@ class FixedDepthAnt(Language):
         )
 
         self.env_dim = env_dim
-        self.n_primitives = n_primitives
+        self.n_primitives = len(primitives)
 
         if structure_params:
-            raise NotImplementedError("Structured params not implemented")
+            raise NotImplementedError("Structured params are not yet implemented")
         self.structure_params = structure_params
+
+        self.primitives = primitives
+
+        # mujoco env
+        self.maze_map = maze_map
+        self.env = gym.make(
+            "AntMaze_UMaze-v4", 
+            maze_map=self.maze_map, 
+            render_mode="rgb_array_list",
+        )
+        self.steps = steps
 
     def _structured_params(self, t: Tree) -> Tuple[np.ndarray, np.ndarray]:
         assert t.value == "root"
@@ -164,9 +139,48 @@ class FixedDepthAnt(Language):
         cond_params, stmt_params = self._structured_params(t)
 
         # simulate program to get weights over primitive policies
-        action = self.fold_eval_E(cond_params, stmt_params, env_state)
+        action_weights = self.fold_eval_E(cond_params, stmt_params, env_state)
 
+        # run mujoco env for `self.steps` steps and collect vector of positions of
+        # ant center of mass
+        obs, info = self.env.reset()
+        step_start = 0
+        episode = 0
+        for step in tqdm(range(100)):
+            pdb.set_trace()
+            primitive_actions = np.stack([
+                pi.act(obs, deterministic=True) for pi in self.primitives
+            ])
+
+            # weighted sum of primitive policies
+            # todo: gumbel softmax
+            action = np.matmul(action_weights, primitive_actions)
+            observation, reward, terminated, truncated, info = env.step(action)
+
+            if terminated or truncated:
+                save_video(
+                    env.render(),
+                    "videos",
+                    fps=env.metadata["render_fps"],
+                    step_starting_index=step_start,
+                    episode_index = episode,
+                )
+                step_start = step + 1
+                episode += 1
+                observation, info = env.reset()
+        
+        save_video(
+            env.render(),
+            "videos",
+            fps=env.metadata["render_fps"],
+            step_starting_index=step_start,
+            episode_index = episode,
+        )
+        env.close()
+
+        # todo: return some feature of the animation
         return action
+
 
     def rec_eval_E(
             self,
@@ -212,26 +226,14 @@ class MujocoAntFeaturizer(Featurizer):
     """
     Ant features: center of gravity along trajectory of ant, i.e. body pos over time?
     """
-    def __init__(self, maze_map: List[List[int]], primitives: np.ndarray, steps: int):
-        assert all(cell == 0 or cell == 1
-                   for row in maze_map for cell in row), \
-                           f"Expected binary map but got {maze_map}"
-
-        self.maze_map = maze_map
-        self.env = gym.make("AntMaze_UMaze-v4", 
-                            maze_map=self.maze_map, 
-                            render_mode="rgb_array_list")
-        self.primitives = primitives
-        self.steps = steps
+    def __init__(self):
+        pass
 
     def apply(self, batch: List[np.ndarray]) -> np.ndarray:
         """
         Translates a series of weights over primitive functions into an ant path
         """
-        print(self.primitives)
-        for p in self.primitives:
-            print(p.shape)
-        pass
+        return batch
 
 
 if __name__ == "__main__":
@@ -240,10 +242,9 @@ if __name__ == "__main__":
         torch.load(f"{primitives_dir}/{direction}.pt").pi
         for direction in ["up", "down", "left", "right"]
     ]
-    torch.save(primitives, "primitives.pt")
-    pdb.set_trace()
-
-    featurizer = MujocoAntFeaturizer(
+    # pdb.set_trace()
+    featurizer = MujocoAntFeaturizer()
+    lang = FixedDepthAnt(
         maze_map=[
             [1, 1, 1, 1, 1],
             [1, 1, 0, 1, 1],
@@ -252,12 +253,9 @@ if __name__ == "__main__":
             [1, 1, 1, 1, 1],
         ],
         primitives=primitives,
-        steps=100,
-    )
-    lang = FixedDepthAnt(
-        env_dim=1,
-        n_primitives=primitives.shape[0],
+        env_dim=111,
         depth=2,
+        steps=100,
         featurizer=featurizer,
     )
     # s = "if (1.0 + [0 1 2] * X >= 0) then [1 2 3] else [4, 5, 6]"
