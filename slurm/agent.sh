8f79bd702d41de7b0348ccc3ca918942bbc77f09
David Jinsoo Lee <djslzx@gmail.com>
2024-04-29 17:11:48 -0400

    add opengl flags

--- slurm/agent.sh
+++ slurm/agent.sh
@@ -3,6 +3,8 @@ PROJECT_DIR="/home/djl328/prob-repl"
 export 'PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:512'
 export 'TOKENIZERS_PARALLELISM=false'
 export PYTHONPATH=$PYTHONPATH:$PROJECT_DIR/src
+export MUJOCO_GL=osmesa
+export PYOPENGL_PLATFORM=osmesa
 source $PROJECT_DIR/venv/bin/activate
 cd $PROJECT_DIR/src
-wandb agent djsl/sft/x2bp6y5g
+wandb agent djsl/dpp-ant/fy8z7poe
