708b7f7d3f81fe9ddb130eaee1c5921b721fa05e
David Jinsoo Lee <djslzx@gmail.com>
2024-05-06 22:21:28 -0400

    mujoco ant tests

--- src/lang/ant.py
+++ src/lang/ant.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 from lang.tree import Language, Tree, Featurizer
 from lang.maze import Maze
 from lang.ant_env import Environment, AntMaze2D
+from lang.mujoco_ant_env import MujocoAntMaze
 import util
 
 
@@ -165,7 +166,11 @@ class FixedDepthAnt(Language):
         conds, stmts = self._extract_params(t)
         obs = self.env.reset()
         outputs = []
-        for _ in range(self.steps):
+
+        steps = range(self.steps)
+        if "load_bar" in env:
+            steps = tqdm(steps)
+        for _ in steps:
             x, y = obs.state
             outputs.append([x, y])
 
@@ -306,16 +311,78 @@ class MultivariateGaussianSampler:
         return self.rv.logpdf(x).item()
 
 
-if __name__ == "__main__":
-    # np.random.seed(0)
-    # ts = util.timestamp()
-    # video_dir = f"videos/{ts}"
-    # util.try_mkdir(video_dir)
+def mujoco_ant_test():
+    ts = util.timestamp()
+    save_dir = f"../../out/mujoco_tests/{ts}"
+    util.try_mkdir(save_dir)
 
-    maze = Maze.from_saved(
-        "lehman-ecj-11-hard"
-        # "cross"
+    maze = Maze.from_saved("lehman-ecj-11-hard")
+    featurizer = EndFeaturizer()
+    mujoco_env = MujocoAntMaze(
+        maze_map=maze,
+        camera_mode="fixed",
+        include_orientation=False,
     )
+    mujoco_ant = FixedDepthAnt(
+        env=mujoco_env,
+        program_depth=6,
+        steps=1000,
+        featurizer=featurizer,
+    )
+
+    simple_env = AntMaze2D(
+        maze_map=maze,
+        step_length=0.05,
+    )
+    simple_ant = FixedDepthAnt(
+        env=simple_env,
+        program_depth=6,
+        steps=1000,
+        featurizer=featurizer,
+    )
+
+    trees = mujoco_ant.samples(n_samples=2, length_cap=10_000)
+
+    mujoco_trails = []
+    simple_trails = []
+    for tree in trees:
+        mujoco_trail = mujoco_ant.eval(tree, env={'load_bar': True})
+        mujoco_env.render_video()
+        mujoco_trails.append(mujoco_trail)
+
+        simple_trail = simple_ant.eval(tree, env={'load_bar': True})
+        simple_trails.append(simple_trail)
+
+    mujoco_trails = np.array(mujoco_trails)
+    maze.plot_trails(mujoco_trails)
+    plt.savefig("mujoco_trails.png")
+    plt.close()
+    np.save(f"{save_dir}/mujoco_trails.npy", mujoco_trails, allow_pickle=True)
+
+    simple_trails = np.array(simple_trails)
+    maze.plot_trails(simple_trails)
+    plt.savefig("simple_trails.png")
+    plt.close()
+    np.save(f"{save_dir}/simple_trails.npy", simple_trails, allow_pickle=True)
+
+
+def mujoco_straight_line_test():
+    """
+    See how straight of a line each primitive policy walks
+    """
+    ants = {
+        "north": """(root (conds [1 0 0 0 -2])
+                          (stmts [1 0 0 0]
+                                 [0 1 0 0]))""",
+        "south": """""",
+        "east": """""",
+        "west": """""",
+    }
+    raise NotImplementedError
+
+
+def simple_ant_test():
+    maze = Maze.from_saved("lehman-ecj-11-hard")
     environment = AntMaze2D(
         maze_map=maze,
         step_length=0.5,
@@ -341,4 +408,15 @@ if __name__ == "__main__":
         trails.append(trail)
 
     maze.plot_trails(np.array(trails))
-    plt.show()
+    plt.savefig("trails.png")
+    plt.close()
+
+
+if __name__ == "__main__":
+    # np.random.seed(0)
+    # ts = util.timestamp()
+    # video_dir = f"videos/{ts}"
+    # util.try_mkdir(video_dir)
+
+    # simple_ant_test()
+    mujoco_ant_test()
