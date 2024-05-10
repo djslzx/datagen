6011d8c05f9974afa02a8bbcff037efc6e0ef27f
David Jinsoo Lee <djslzx@gmail.com>
2024-05-10 01:00:10 -0400

    add lsys, ant slurm scripts

---
+++ slurm/lsys.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PROJECT_DIR="/home/djl328/prob-repl"
+export 'PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:512'
+export 'TOKENIZERS_PARALLELISM=false'
+export PYTHONPATH=$PYTHONPATH:$PROJECT_DIR/src
+export MUJOCO_GL=osmesa
+export PYOPENGL_PLATFORM=osmesa
+source $PROJECT_DIR/venv/bin/activate
+cd $PROJECT_DIR/src
+wandb agent djsl/dpp-lsys/0fx5hy5b
